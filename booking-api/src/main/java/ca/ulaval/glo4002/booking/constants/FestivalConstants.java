d7a3696d547a3251c68a70177493abaf1079bab0
Guillaume Begin <guillaume.begin.3@ulaval.ca>
2019-09-24 14:53:59 -0400

    typo

--- booking-api/src/main/java/ca/ulaval/glo4002/booking/constants/FestivalConstants.java
+++ booking-api/src/main/java/ca/ulaval/glo4002/booking/constants/FestivalConstants.java
@@ -5,7 +5,7 @@ import java.time.LocalDate;
 public class FestivalConstants {
 
     public static class DateFestival {
-        public static final LocalDate FESTIVAL_START = LocalDate.of(2050, 7, 17);
-        public static final LocalDate FESTIVAL_END = LocalDate.of(2050, 7, 24);
+        public static final LocalDate FESTIVAL_START_DATE = LocalDate.of(2050, 7, 17);
+        public static final LocalDate FESTIVAL_END_DATE = LocalDate.of(2050, 7, 24);
     }
 }
