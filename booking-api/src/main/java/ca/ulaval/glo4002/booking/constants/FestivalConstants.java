217e66afdf2076a8d514e7aa4c061a28dd1cb931
Guillaume Begin <guillaume.begin.3@ulaval.ca>
2019-09-24 14:52:47 -0400

    Added constants for festival start date and festival end date

---
+++ booking-api/src/main/java/ca/ulaval/glo4002/booking/constants/FestivalConstants.java
@@ -0,0 +1,11 @@
+package ca.ulaval.glo4002.booking.constants;
+
+import java.time.LocalDate;
+
+public class FestivalConstants {
+
+    public static class DateFestival {
+        public static final LocalDate FESTIVAL_START = LocalDate.of(2050, 7, 17);
+        public static final LocalDate FESTIVAL_END = LocalDate.of(2050, 7, 24);
+    }
+}
