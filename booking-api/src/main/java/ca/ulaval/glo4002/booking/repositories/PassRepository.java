f705d3e9736a15b5575ec3e93b6fa74a739ba863
Guillaume Begin <guillaume.begin.3@ulaval.ca>
2019-09-24 01:31:52 -0400

    Added base oxygenRepo and PassRepo

---
+++ booking-api/src/main/java/ca/ulaval/glo4002/booking/repositories/PassRepository.java
@@ -0,0 +1,8 @@
+package ca.ulaval.glo4002.booking.repositories;
+
+import ca.ulaval.glo4002.booking.entities.passes.Pass;
+import org.springframework.data.repository.CrudRepository
+
+public interface PassRepository extends CrudRepository<Pass, Long> {
+
+}
