46c84013e6832a26fc1b1360b97c4d6b44e7d3ad
Alexandre Boily <alexandre.boily.10@ulaval.ca>
2019-09-26 14:21:30 -0400

    Created FestivalException and changed inheritance of all custom exceptions.

--- booking-api/src/main/java/ca/ulaval/glo4002/booking/exceptions/shuttles/ShuttleCategoryNotFoundException.java
+++ booking-api/src/main/java/ca/ulaval/glo4002/booking/exceptions/shuttles/ShuttleCategoryNotFoundException.java
@@ -1,11 +1,12 @@
 package ca.ulaval.glo4002.booking.exceptions.shuttles;
 
 import ca.ulaval.glo4002.booking.constants.ExceptionConstants;
+import ca.ulaval.glo4002.booking.exceptions.FestivalException;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(HttpStatus.NOT_FOUND)
-public class ShuttleCategoryNotFoundException extends RuntimeException {
+public class ShuttleCategoryNotFoundException extends FestivalException {
 
 	public ShuttleCategoryNotFoundException() {
 		super(ExceptionConstants.SHUTTLE_CATEGORY_NOT_FOUND_MESSAGE);
