75b61b8275d76f9adaa654b4e43aab415945de54
Guillaume Begin <guillaume.begin.3@ulaval.ca>
2019-09-28 17:47:38 -0400

    fixes and test

--- booking-api/src/main/java/ca/ulaval/glo4002/booking/entities/oxygen/OxygenTankInventory.java
+++ booking-api/src/main/java/ca/ulaval/glo4002/booking/entities/oxygen/OxygenTankInventory.java
@@ -6,41 +6,55 @@ import ca.ulaval.glo4002.booking.exceptions.oxygen.OxygenCategoryNotFoundExcepti
 import java.util.HashMap;
 import java.util.Map;
 
+import static java.lang.Math.abs;
 import static java.util.Optional.ofNullable;
 
 public class OxygenTankInventory {
     private Map<Long, Long> inventory = new HashMap<>();
-    private Map<Long, Long> inUseTank = new HashMap<>();
+    private Map<Long, Long> tankInUse = new HashMap<>();
 
     public OxygenTankInventory() {
         inventory.put(OxygenConstants.Categories.E_ID, 0L);
-        inUseTank.put(OxygenConstants.Categories.E_ID, 0L);
+        tankInUse.put(OxygenConstants.Categories.E_ID, 0L);
         inventory.put(OxygenConstants.Categories.B_ID, 0L);
-        inUseTank.put(OxygenConstants.Categories.B_ID, 0L);
-        inUseTank.put(OxygenConstants.Categories.A_ID, 0L);
+        tankInUse.put(OxygenConstants.Categories.B_ID, 0L);
+        tankInUse.put(OxygenConstants.Categories.A_ID, 0L);
         inventory.put(OxygenConstants.Categories.A_ID, 0L);
     }
 
 
 
-    public void addTankInInventory(Long category, Long quantity) {
-        ofNullable(inventory.replace(category, inventory.get(category) + quantity)).orElseThrow(OxygenCategoryNotFoundException::new);
+    public void addTankInInventory(Long categoryID, Long quantityAdded) {
+        Long quantityStored = ofNullable(inventory.get(categoryID))
+                .orElseThrow(OxygenCategoryNotFoundException::new);
+
+        inventory.replace(categoryID, quantityStored + quantityAdded);
     }
 
-    public boolean requestOxygenTank(Long categoryID, Long quantityRequested) {;
-        Long quantityStored = ofNullable(inventory.get(categoryID)).orElseThrow(OxygenCategoryNotFoundException::new);
-        Long quantityInUse = ofNullable(inventory.get(categoryID)).orElseThrow(OxygenCategoryNotFoundException::new);
+    public Long requestOxygenTank(Long categoryID, Long quantityRequested) {;
+        Long quantityStored = ofNullable(inventory.get(categoryID))
+                .orElseThrow(OxygenCategoryNotFoundException::new);
+        Long quantityInUse = ofNullable(tankInUse.get(categoryID))
+                .orElseThrow(OxygenCategoryNotFoundException::new);
 
-        if(quantityStored - quantityInUse + quantityRequested < 0){
-            return false;
+        Long numberOfTankNeeded = quantityStored - (quantityInUse + quantityRequested);
+        if(numberOfTankNeeded < 0) {
+            tankInUse.replace(categoryID, quantityStored);
+            return abs(numberOfTankNeeded);
         }
         else {
-            inUseTank.replace(categoryID, quantityInUse + quantityRequested);
-            return true;
+            tankInUse.replace(categoryID, quantityInUse + quantityRequested);
+            return 0L;
         }
     }
 
     public Long getInventoryByCategoryID(Long categoryID) {
-        return ofNullable(inventory.get(categoryID)).orElseThrow(OxygenCategoryNotFoundException::new);
+        return ofNullable(inventory.get(categoryID))
+                .orElseThrow(OxygenCategoryNotFoundException::new);
+    }
+
+    public long getTankInUseByCategoryID(Long categoryID) {
+        return ofNullable(tankInUse.get(categoryID))
+                .orElseThrow(OxygenCategoryNotFoundException::new);
     }
 }
