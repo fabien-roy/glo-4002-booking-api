a3a2f92d9af84dca78350d77f8c97a9d35a47141
Brian Cloutier <brian.cloutier@hotmail.com>
2019-09-28 14:14:40 -0400

    Added invalid event date exception

---
+++ booking-api/src/main/java/ca/ulaval/glo4002/booking/exceptions/InvalidDateException.java
@@ -0,0 +1,13 @@
+package ca.ulaval.glo4002.booking.exceptions;
+
+import javax.ws.rs.core.Response;
+
+import ca.ulaval.glo4002.booking.constants.ExceptionConstants;
+
+public class InvalidDateException extends RuntimeException {
+	
+	public InvalidDateException(Response response) {
+		super(ExceptionConstants.INVALID_EVENT_DATE_MESSAGE);
+	}
+
+}
