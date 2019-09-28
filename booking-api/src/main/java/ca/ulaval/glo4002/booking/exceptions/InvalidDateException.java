8f54f3fce2cf8529fb6d3d6f84da0610b851bd65
Brian Cloutier <brian.cloutier@hotmail.com>
2019-09-28 14:42:27 -0400

    Changed response handling in invalid date exception

--- booking-api/src/main/java/ca/ulaval/glo4002/booking/exceptions/InvalidDateException.java
+++ booking-api/src/main/java/ca/ulaval/glo4002/booking/exceptions/InvalidDateException.java
@@ -6,7 +6,7 @@ import ca.ulaval.glo4002.booking.constants.ExceptionConstants;
 
 public class InvalidDateException extends RuntimeException {
 	
-	public InvalidDateException(Response response) {
+	public InvalidDateException(Response.Status response) {
 		super(ExceptionConstants.INVALID_EVENT_DATE_MESSAGE);
 	}
 
