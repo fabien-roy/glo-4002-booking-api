7a23f7a6151ae35ccb7874ebcedbd9fbb5b87df0
Brian Cloutier <brian.cloutier@hotmail.com>
2019-09-28 15:13:14 -0400

    Added TODO comments prior to PR

--- booking-api/src/main/java/ca/ulaval/glo4002/booking/controllers/ShuttleManifestController.java
+++ booking-api/src/main/java/ca/ulaval/glo4002/booking/controllers/ShuttleManifestController.java
@@ -1,12 +1,14 @@
 package ca.ulaval.glo4002.booking.controllers;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.Status;
 
 import ca.ulaval.glo4002.booking.entities.shuttles.ShuttleManifest;
@@ -16,16 +18,16 @@ import ca.ulaval.glo4002.booking.validators.FestivalDateValidator;
 @Path("/shuttle-manifests")
 @Produces(MediaType.APPLICATION_JSON)
 public class ShuttleManifestController {
-	
+	//TODO : Return a real shuttle manifest using a manifest builder
 	@GET
-	public ShuttleManifest getShuttleManifests(@QueryParam("date") String date) {
+	public Response getShuttleManifests(@QueryParam("date") String date) {
 		LocalDate manifestDate = LocalDate.parse(date);
 		
 		if (!FestivalDateValidator.dateIsOutsideFestivalDates(manifestDate)) {
-			
+			//TODO : manifest generation logic
 		} else throw new InvalidDateException(Status.BAD_REQUEST);
 		
-		return null;
+		return Response.ok().entity(new ShuttleManifest(manifestDate, new ArrayList<>(), new ArrayList<>())).build();
 		
 	}
 
