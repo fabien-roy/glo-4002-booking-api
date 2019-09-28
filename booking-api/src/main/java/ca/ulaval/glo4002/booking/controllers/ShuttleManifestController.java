5689797acd37b40099a664e7b59b3effc046dd29
Brian Cloutier <brian.cloutier@hotmail.com>
2019-09-28 14:41:09 -0400

    Added date validation in manifest controller

--- booking-api/src/main/java/ca/ulaval/glo4002/booking/controllers/ShuttleManifestController.java
+++ booking-api/src/main/java/ca/ulaval/glo4002/booking/controllers/ShuttleManifestController.java
@@ -1,22 +1,38 @@
 package ca.ulaval.glo4002.booking.controllers;
 
-import java.util.Date;
+import java.time.LocalDate;
 
+import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
-import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
 
-import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.MediaType;
 
+import ca.ulaval.glo4002.booking.constants.FestivalConstants;
+import ca.ulaval.glo4002.booking.exceptions.InvalidDateException;
+import ca.ulaval.glo4002.booking.responses.ShuttleManifestResponse;
+
 @Path("/shuttle-manifests")
-@Produces(MediaType.APPLICATION_JSON)
 public class ShuttleManifestController {
 	
-	public Response getShuttleManifests(@QueryParam("date") @DateTimeFormat
-			(iso = DateTimeFormat.ISO) LocalDate date) {
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	public ShuttleManifestResponse getShuttleManifests(@QueryParam("date") String date) {
+		LocalDate manifestDate = LocalDate.parse(date);
 		
+		if (!dateIsOutsideFestivalDates(manifestDate)) {
+			
+		} else throw new InvalidDateException(Status.BAD_REQUEST);
+		
+		return null;
+		
+	}
+	
+	public boolean dateIsOutsideFestivalDates(LocalDate date) {
+		return date.isBefore(FestivalConstants.Dates.START_DATE) 
+				|| date.isAfter(FestivalConstants.Dates.END_DATE);
 	}
 
 }
