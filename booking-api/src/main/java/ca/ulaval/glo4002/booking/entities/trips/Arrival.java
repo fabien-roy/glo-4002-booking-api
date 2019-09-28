d5f3b80d5b870f0dea9dd48ec2f7c193a943c3ac
Brian Cloutier <brian.cloutier@hotmail.com>
2019-09-28 13:33:10 -0400

    Added trips, arrivals and departures entities

---
+++ booking-api/src/main/java/ca/ulaval/glo4002/booking/entities/trips/Arrival.java
@@ -0,0 +1,18 @@
+package ca.ulaval.glo4002.booking.entities.trips;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import ca.ulaval.glo4002.booking.entities.shuttles.categories.ShuttleCategory;
+
+public class Arrival extends Trip {
+
+	public Arrival(LocalDate date, List<Long> passengers, ShuttleCategory shuttleName) {
+		this.date = date;
+		this.passengers = passengers;
+		this.shuttleName = shuttleName;
+	}
+	
+	
+
+}
---
+++ booking-api/src/main/java/ca/ulaval/glo4002/booking/entities/trips/Departure.java
@@ -0,0 +1,17 @@
+package ca.ulaval.glo4002.booking.entities.trips;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import ca.ulaval.glo4002.booking.entities.shuttles.categories.ShuttleCategory;
+
+public class Departure extends Trip {
+	
+	public Departure(LocalDate date, List<Long> passengers, ShuttleCategory shuttleName) {
+		this.date = date;
+		this.passengers = passengers;
+		this.shuttleName = shuttleName;
+		
+	}
+
+}
