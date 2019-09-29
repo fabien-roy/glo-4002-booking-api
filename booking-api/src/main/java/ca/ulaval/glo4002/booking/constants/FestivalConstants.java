733fc315557a0296d9147164ea2ede508da1d607
Guillaume Begin <guillaume.begin.3@ulaval.ca>
2019-09-29 01:32:17 -0400

    Moved getOxygenCategoryForTimeTable to OxygenTankServices

--- booking-api/src/main/java/ca/ulaval/glo4002/booking/constants/FestivalConstants.java
+++ booking-api/src/main/java/ca/ulaval/glo4002/booking/constants/FestivalConstants.java
@@ -10,7 +10,6 @@ public class FestivalConstants {
         private final static DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern(FestivalConstants.Dates.FORMAT);
         public static final LocalDate START_DATE = LocalDate.parse("2050-07-17", dateTimeFormatter);
         public static final LocalDate END_DATE = LocalDate.parse("2050-07-24", dateTimeFormatter);
-        public static final Long TOTAL_DURATION_IN_DAYS = 8L;
         public static final String FORMAT = "yyyy-MM-dd";
 
         private Dates() {
--- booking-api/src/main/java/ca/ulaval/glo4002/booking/entities/oxygen/OxygenTank.java
+++ booking-api/src/main/java/ca/ulaval/glo4002/booking/entities/oxygen/OxygenTank.java
@@ -6,6 +6,7 @@ import ca.ulaval.glo4002.booking.constants.OxygenConstants;
 import ca.ulaval.glo4002.booking.entities.orders.OrderItem;
 import ca.ulaval.glo4002.booking.entities.oxygen.categories.OxygenCategory;
 import ca.ulaval.glo4002.booking.exceptions.InvalidEventDateException;
+import ca.ulaval.glo4002.booking.services.OxygenTankServices;
 import org.springframework.data.annotation.Id;
 
 import javax.persistence.Entity;
@@ -22,13 +23,14 @@ public class OxygenTank extends OrderItem {
 	private OxygenCategory category;
 	private LocalDate timeProduced;
 	private LocalDate timeRequested;
+	private OxygenTankServices oxygenTankServices = new OxygenTankServices();
 
 	public OxygenTank(OxygenCategory category, LocalDate timeRequested) {
 		if(timeRequested.isAfter(FestivalConstants.Dates.START_DATE)) {
 			throw new InvalidEventDateException();
 		}
 		
-		this.category = getOxygenCategoryForTimeTable(category, timeRequested);
+		this.category =oxygenTankServices.getOxygenCategoryForTimeTable(category, timeRequested);
 		this.timeRequested = timeRequested;
 		this.timeProduced = timeRequested.plusDays(this.category.getProduction().getProductionTime().toDays());
 	}
@@ -48,22 +50,6 @@ public class OxygenTank extends OrderItem {
 
 	public LocalDate getTimeProduced() {
 		return timeProduced;
-	}
-
-	private OxygenCategory getOxygenCategoryForTimeTable(OxygenCategory category, LocalDate timeRequested) {
-		Long timeToProduce = category.getProduction().getProductionTime().toDays();
-		LocalDate timeReady = timeRequested.plusDays(timeToProduce);
-		OxygenCategoryBuilder oxygenCategoryBuilder = new OxygenCategoryBuilder();
-
-		if(timeReady.isBefore(FestivalConstants.Dates.START_DATE)) {
-			return category;
-		}
-		else if(timeRequested.plus(10, DAYS).isBefore(FestivalConstants.Dates.START_DATE)){
-			return  oxygenCategoryBuilder.buildById(OxygenConstants.Categories.B_ID);
-		}
-		else {
-			return oxygenCategoryBuilder.buildById(OxygenConstants.Categories.E_ID);
-		}
 	}
 
 }
