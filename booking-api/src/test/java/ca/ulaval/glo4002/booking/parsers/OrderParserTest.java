d9bc2a26cccb176ba5eba7ca49f99e01179e31ce
Fabien Roy <fabienroy28@gmail.com>
2019-09-30 14:56:42 -0400

    Add OrderParser tests

--- booking-api/src/test/java/ca/ulaval/glo4002/booking/parsers/OrderParserTest.java
+++ booking-api/src/test/java/ca/ulaval/glo4002/booking/parsers/OrderParserTest.java
@@ -1,10 +1,12 @@
 package ca.ulaval.glo4002.booking.parsers;
 
+import ca.ulaval.glo4002.booking.builders.VendorBuilder;
 import ca.ulaval.glo4002.booking.constants.ExceptionConstants;
 import ca.ulaval.glo4002.booking.constants.FestivalConstants;
 import ca.ulaval.glo4002.booking.constants.VendorConstants;
 import ca.ulaval.glo4002.booking.domainObjects.orders.Order;
 import ca.ulaval.glo4002.booking.dto.OrderDto;
+import ca.ulaval.glo4002.booking.entities.OrderEntity;
 import ca.ulaval.glo4002.booking.exceptions.VendorNotFoundException;
 import ca.ulaval.glo4002.booking.exceptions.orders.OrderDtoInvalidException;
 import ca.ulaval.glo4002.booking.util.FestivalDateUtil;
@@ -17,6 +19,7 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class OrderParserTest {
 
+    private static final Long A_VALID_ID = 1L;
     private static final String A_DATE_BEFORE_ORDER_START_DATE_TIME = FestivalConstants.Dates.ORDER_START_DATE_TIME.minusDays(1).toString();
     private static final String A_DATE_AFTER_ORDER_START_END_TIME = FestivalConstants.Dates.ORDER_END_DATE_TIME.plusDays(1).toString();
     private static final LocalDateTime A_VALID_DATE = FestivalConstants.Dates.ORDER_START_DATE_TIME;
@@ -24,12 +27,20 @@ class OrderParserTest {
     private static final String A_VALID_VENDOR_CODE = VendorConstants.TEAM_VENDOR_CODE;
     private OrderParser subject;
     private OrderDto orderDto = new OrderDto();
+    private Order order;
+    private VendorBuilder vendorBuilder = new VendorBuilder();
 
     @BeforeEach
     void setUp() {
         subject = new OrderParser();
         orderDto.orderDate = FestivalDateUtil.toZonedDateTimeString(A_VALID_DATE);
         orderDto.vendorCode = A_VALID_VENDOR_CODE;
+
+        order = new Order(
+                A_VALID_ID,
+                A_VALID_DATE,
+                vendorBuilder.buildByCode(A_VALID_VENDOR_CODE)
+        );
     }
 
     @Test
@@ -87,9 +98,32 @@ class OrderParserTest {
         assertEquals(A_VALID_VENDOR_CODE, order.getVendor().getCode());
     }
 
-    // TODO : Test parseEntity
+    @Test
+    void whenParsingEntity_orderShouldBeValid() {
+        OrderEntity entity = subject.toEntity(order);
 
-    // TODO : Test toEntity
+        Order parsedOrder = subject.parseEntity(entity);
 
-    // TODO : Test toDto
+        assertEquals(entity.id, parsedOrder.getId());
+        assertEquals(entity.vendorId, parsedOrder.getVendor().getId());
+        assertEquals(entity.orderDate, parsedOrder.getOrderDate());
+    }
+
+    @Test
+    void whenParsingToEntity_entityShouldBeValid() {
+        OrderEntity entity = subject.toEntity(order);
+
+        assertEquals(order.getId(), entity.id);
+        assertEquals(order.getVendor().getId(), entity.vendorId);
+        assertEquals(order.getOrderDate(), entity.orderDate);
+    }
+
+    @Test
+    void whenParsingToDto_dtoShouldBeValid() {
+        OrderDto dto = subject.toDto(order);
+
+        assertEquals(order.getId(), dto.orderNumber);
+        assertEquals(order.getVendor().getCode(), dto.vendorCode);
+        assertEquals(order.getOrderDate().toString(), A_VALID_DATE.toString());
+    }
 }
