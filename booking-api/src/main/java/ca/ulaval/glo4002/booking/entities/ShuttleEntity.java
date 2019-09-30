003c9da8718f13d6b09d7428b6510d0c8b1d4fcd
Brian Cloutier <brian.cloutier@hotmail.com>
2019-09-30 15:01:30 -0400

    Added list of trips to shuttle and entity

--- booking-api/src/main/java/ca/ulaval/glo4002/booking/entities/ShuttleEntity.java
+++ booking-api/src/main/java/ca/ulaval/glo4002/booking/entities/ShuttleEntity.java
@@ -1,9 +1,14 @@
 package ca.ulaval.glo4002.booking.entities;
 
+import java.util.List;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+import ca.ulaval.glo4002.booking.domainObjects.trips.Trip;
 
 @Entity(name = "shuttles")
 public class ShuttleEntity {
@@ -17,19 +22,30 @@ public class ShuttleEntity {
 	//@ManyToOne(cascade = CascadeType.ALL)
 	//private List<Passenger> passengers;
 	
+	@OneToMany(mappedBy = "shuttle")
+	private List<Trip> trips;
+	
 	public ShuttleEntity() {
 		
 	}
 
-	public ShuttleEntity(Long id, Long shuttleCategoryId, Long shuttleTypeId) {
-		super();
+	public ShuttleEntity(Long id, Long shuttleCategoryId, Long shuttleTypeId, List<Trip> trips) {
 		this.id = id;
 		this.shuttleCategoryId = shuttleCategoryId;
 		this.shuttleTypeId = shuttleTypeId;
+		this.trips = trips;
 	}
 	
 	public Long getId() {
 		return id;
+	}
+	
+	public Long getShuttleCategoryId() {
+		return shuttleCategoryId;
+	}
+	
+	public Long getShuttleTypeId() {
+		return shuttleTypeId;
 	}
 	
 
