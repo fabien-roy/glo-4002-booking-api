5e4cd62231ebef8b4ed733c912a124b4efbb5e19
Brian Cloutier <brian.cloutier@hotmail.com>
2019-09-30 10:10:24 -0400

    Removed @Id annotation in domain object

--- booking-api/src/main/java/ca/ulaval/glo4002/booking/domainObjects/shuttles/Shuttle.java
+++ booking-api/src/main/java/ca/ulaval/glo4002/booking/domainObjects/shuttles/Shuttle.java
@@ -1,18 +1,13 @@
 package ca.ulaval.glo4002.booking.domainObjects.shuttles;
 import java.util.List;
 
-import javax.persistence.Entity;
-
-import org.springframework.data.annotation.Id;
-
 import ca.ulaval.glo4002.booking.domainObjects.orders.OrderItem;
 import ca.ulaval.glo4002.booking.domainObjects.shuttles.categories.ShuttleCategory;
 import ca.ulaval.glo4002.booking.domainObjects.shuttles.types.ShuttleType;
 import ca.ulaval.glo4002.booking.exceptions.shuttles.ShuttleFullException;
 
 public class Shuttle extends OrderItem {
-	
-	@Id
+
 	protected Long id;
 
 	private Double price;
