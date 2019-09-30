8803f0e32a9686f5e4b78554d0b1c792b340b09c
Brian Cloutier <brian.cloutier@hotmail.com>
2019-09-30 10:08:45 -0400

    Added shuttle entity

---
+++ booking-api/src/main/java/ca/ulaval/glo4002/booking/entities/ShuttleEntity.java
@@ -0,0 +1,32 @@
+package ca.ulaval.glo4002.booking.entities;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity(name = "shuttles")
+public class ShuttleEntity {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private Long shuttleCategoryId;
+	private Long shuttleTypeId;
+	
+	//@ManyToOne(cascade = CascadeType.ALL)
+	//private List<Passenger> passengers;
+	
+	public ShuttleEntity() {
+		
+	}
+
+	public ShuttleEntity(Long id, Long shuttleCategoryId, Long shuttleTypeId) {
+		super();
+		this.id = id;
+		this.shuttleCategoryId = shuttleCategoryId;
+		this.shuttleTypeId = shuttleTypeId;
+	}
+	
+
+}
