cb8c8212f1da089f056c6965507e3edc4a97f246
Brian Cloutier <brian.cloutier@hotmail.com>
2019-09-30 10:09:03 -0400

    Added shuttle service

---
+++ booking-api/src/main/java/ca/ulaval/glo4002/booking/services/ShuttleService.java
@@ -0,0 +1,7 @@
+package ca.ulaval.glo4002.booking.services;
+
+import ca.ulaval.glo4002.booking.domainObjects.shuttles.Shuttle;
+
+public interface ShuttleService extends Service<Shuttle, Long>{
+
+}
