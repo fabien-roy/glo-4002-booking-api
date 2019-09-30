e19ffa056b04b6c72d779f0e1608c1edaa60c74a
Brian Cloutier <brian.cloutier@hotmail.com>
2019-09-30 10:36:46 -0400

    Added shuttle service impl stub

--- booking-api/src/main/java/ca/ulaval/glo4002/booking/services/ShuttleServiceImpl.java
+++ booking-api/src/main/java/ca/ulaval/glo4002/booking/services/ShuttleServiceImpl.java
@@ -1,5 +1,41 @@
 package ca.ulaval.glo4002.booking.services;
 
-public class ShuttleServiceImpl {
+import ca.ulaval.glo4002.booking.domainObjects.shuttles.Shuttle;
+import ca.ulaval.glo4002.booking.repositories.ShuttleRepository;
+
+public class ShuttleServiceImpl implements ShuttleService {
+	
+	private final ShuttleRepository shuttleRepository;
+	
+	
+	public ShuttleServiceImpl(ShuttleRepository shuttleRepository) {
+		this.shuttleRepository = shuttleRepository;
+	}
+
+	@Override
+	public Shuttle findById(Long id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Iterable<Shuttle> findAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Shuttle save(Shuttle object) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Iterable<Shuttle> saveAll(Iterable<Shuttle> objects) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	
 
 }
