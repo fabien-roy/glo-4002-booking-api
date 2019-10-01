0cb15b94da7616125b5505c22bcaa41fefd9bed9
Fabien Roy <fabienroy28@gmail.com>
2019-09-30 21:06:28 -0400

    Add trip repository test

--- booking-api/src/test/java/ca/ulaval/glo4002/booking/repositories/TripRepositoryTest.java
+++ booking-api/src/test/java/ca/ulaval/glo4002/booking/repositories/TripRepositoryTest.java
@@ -1,6 +1,14 @@
 package ca.ulaval.glo4002.booking.repositories;
 
+import ca.ulaval.glo4002.booking.entities.TripEntity;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 // TODO : Solve persistence problem
 
@@ -15,5 +23,14 @@ public class TripRepositoryTest {
         subject = new TripRepositoryImpl(context.entityManager);
     }
 
-    // TODO
+    @Test
+    public void findAll_shouldReturnCorrectTrips() {
+        List<TripEntity> trips = new ArrayList<>();
+
+        subject.findAll().forEach(trips::add);
+
+        assertEquals(2, trips.size());
+        assertTrue(trips.contains(context.aTrip));
+        assertTrue(trips.contains(context.anotherTrip));
+    }
 }
