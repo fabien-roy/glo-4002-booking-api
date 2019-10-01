69354c8c51432e1538508bf5d67749dd497e6c02
Guillaume Begin <guillaume.begin.3@ulaval.ca>
2019-10-01 03:35:57 -0400

    Comment on refactor if we don't follow TDA on requestOxygenTank

--- booking-api/src/main/java/ca/ulaval/glo4002/booking/domainobjects/report/Inventory.java
+++ booking-api/src/main/java/ca/ulaval/glo4002/booking/domainobjects/report/Inventory.java
@@ -30,6 +30,20 @@ public class Inventory {
         inventory.replace(categoryID, quantityStored + quantityAdded);
     }
 
+    // TODO : to refactor completly.
+    // Si on part d'un order (vraiment pas une bonne idée) train de getter de la mort
+    //Long numberOfTankPerDays = (order.getOrderItems().at(0).getCategory().getQuality().getOxygenTanksNeededPerDay())
+    // Si package
+    //Long quantityRequested = (END_DATE - START_DATE) * numberOfTankPerDays
+    // Si SinglePass
+    //Long quantityRequested = order.getOrderItems().size() * numberOfTankPerDays
+    // Si on est capable d'avoir la category d'oxygène à partir d'un order (pas le cas présentement)
+    //numberOfTankProducePerDays = order.getOrderItems.at(0).getOxygenCategory().getProduction().getProducedUnit()
+    // Multiple de (1,3,5) sinon + 1
+    //numberOfExpectedConstructorCall = (quantityRequested % numberOfTankProducePerDays == 0 ? quantityRequested / numberOfTankProducePerDays : quantityRequested / numberOfTankProducePerDays + 1)
+    //Il faut tenir compte de l'inventaire non utilisé donc
+    //realQuantityNeeded = abs(quantityStored - (quantityInUse + quantityRequested))
+    //numberOfRealConstructorCall = (realQuantityNeeded % numberOfTankProducePerDays == 0 ? realQuantityNeeded / numberOfTankProducePerDays : realQuantityNeeded / numberOfTankProducePerDays + 1)
     public Long requestOxygenTank(Long categoryID, Long quantityRequested) {
         Long quantityStored = ofNullable(inventory.get(categoryID))
                 .orElseThrow(OxygenCategoryNotFoundException::new);
