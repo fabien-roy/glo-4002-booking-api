d6470c97ad84f01cb0bf337dc348fce324131b9d
Brian Cloutier <brian.cloutier@hotmail.com>
2019-10-01 15:25:34 -0400

    Added constructor without date for cos 5

--- booking-api/src/main/java/ca/ulaval/glo4002/booking/domainobjects/shuttles/ShuttleManifest.java
+++ booking-api/src/main/java/ca/ulaval/glo4002/booking/domainobjects/shuttles/ShuttleManifest.java
@@ -17,6 +17,11 @@ public class ShuttleManifest {
 		this.arrivals = arrivals;
 		this.tripDate = date;
 	}
+	
+	public ShuttleManifest(List<DepartureTrip> departures, List<ArrivalTrip> arrivals) {
+		this.departures = departures;
+		this.arrivals = arrivals;
+	}
 
 	public List<DepartureTrip> getDepartures() {
 		return departures;
