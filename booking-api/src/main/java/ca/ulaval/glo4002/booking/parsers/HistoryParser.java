399ee61d0351426e96e314218efe3e7671d51ba8
Fabien Roy <fabienroy28@gmail.com>
2019-10-02 18:13:26 -0400

    Repair build

--- booking-api/src/main/java/ca/ulaval/glo4002/booking/parsers/HistoryParser.java
+++ booking-api/src/main/java/ca/ulaval/glo4002/booking/parsers/HistoryParser.java
@@ -2,17 +2,17 @@ package ca.ulaval.glo4002.booking.parsers;
 
 import ca.ulaval.glo4002.booking.domainobjects.report.History;
 import ca.ulaval.glo4002.booking.dto.HistoryItemDto;
+import ca.ulaval.glo4002.booking.exceptions.UnusedMethodException;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class HistoryParser implements DtoParser<History, List<HistoryItemDto>>{
 
+    // TODO : OXY : Will we really have to parse a History Dto??
     @Override
     public History parseDto(List<HistoryItemDto> dto) {
-        // TODO : OXY
-
-        return new History();
+        throw new UnusedMethodException();
     }
 
     @Override
