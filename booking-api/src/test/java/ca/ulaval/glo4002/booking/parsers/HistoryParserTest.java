f8fac5dccbdf9059a47ff9a3354076b226ac172c
MonsterX9797 <m-cgo@hotmail.com>
2019-10-02 11:58:17 -0400

    setup HistoryParserTEst

---
+++ booking-api/src/test/java/ca/ulaval/glo4002/booking/parsers/HistoryParserTest.java
@@ -0,0 +1,30 @@
+package ca.ulaval.glo4002.booking.parsers;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class HistoryParserTest {
+
+	private HistoryParser subject;
+
+	@BeforeEach
+	void setUp() {
+		subject = new HistoryParser();
+		// TODO Set up whats left to setup
+	}
+
+	// TODO do this test
+	@Test
+	void whenParsingDto_historyShouldBeValid() {
+		fail("Not yet implemented");
+	}
+
+	// TODO do this test
+	@Test
+	void whenParsingToDto_shouldReturnUnusedMethodException() {
+		fail("Not yet implemented");
+	}
+
+}
