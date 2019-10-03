96073542bc5c36170db8101dbac9b54297197481
Guillaume Begin <guillaume.begin.3@ulaval.ca>
2019-10-03 18:47:16 -0400

    added add fonction in InventoryEntity

--- booking-api/src/main/java/ca/ulaval/glo4002/booking/entities/InventoryEntity.java
+++ booking-api/src/main/java/ca/ulaval/glo4002/booking/entities/InventoryEntity.java
@@ -11,22 +11,28 @@ public class InventoryEntity {
     @GeneratedValue(strategy = GenerationType.AUTO)
     @Column(updatable = false, nullable = false)
 	private Long id;
-    @OneToMany(mappedBy = "inventory", cascade = CascadeType.ALL)
+    @OneToMany(mappedBy = "inventory")
     private List<InventoryItemEntity> inUseTanks = new ArrayList<>();
-    @OneToMany(mappedBy = "inventory", cascade = CascadeType.ALL)
+    @OneToMany(mappedBy = "inventory")
     private List<InventoryItemEntity> notInUseTanks = new ArrayList<>();
 
     public InventoryEntity() {
 
     }
 
-    public InventoryEntity(Long id) {
-        this.id = id;
-    }
-
     public InventoryEntity(List<InventoryItemEntity> inUseTanks, List<InventoryItemEntity> notInUseTanks) {
         this.inUseTanks = inUseTanks;
         this.notInUseTanks = notInUseTanks;
+    }
+
+    public InventoryEntity(Long id, List<InventoryItemEntity> inUseTanks, List<InventoryItemEntity> notInUseTanks) {
+        this.id = id;
+        this.inUseTanks = inUseTanks;
+        this.notInUseTanks = notInUseTanks;
+    }
+
+    public Long getId() {
+        return id;
     }
 
     public void setId(Long id) {
@@ -44,6 +50,14 @@ public class InventoryEntity {
     public void clearInventoryItems() {
         inUseTanks.clear();
         notInUseTanks.clear();
+    }
+
+    public void addInUseTank(List<InventoryItemEntity> inUseTanks) {
+        this.inUseTanks = inUseTanks;
+    }
+
+    public void addNotInUseTank(List<InventoryItemEntity> notInUseTanks) {
+        this.notInUseTanks = notInUseTanks;
     }
 
     public void updateInUseTanks(List<InventoryItemEntity> inventoryItemEntities) {
