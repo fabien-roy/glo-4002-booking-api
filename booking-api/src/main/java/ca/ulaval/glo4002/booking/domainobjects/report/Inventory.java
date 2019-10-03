33286d42a4b0986ff2684364c52cb60d74f5c1c2
Guillaume Begin <guillaume.begin.3@ulaval.ca>
2019-10-03 10:52:44 -0400

    Restored Inventory to is previous nature

--- booking-api/src/main/java/ca/ulaval/glo4002/booking/domainobjects/report/Inventory.java
+++ booking-api/src/main/java/ca/ulaval/glo4002/booking/domainobjects/report/Inventory.java
@@ -7,23 +7,44 @@ import java.util.Map;
 
 public class Inventory {
 
-    private Map<Long, Long> oxygenTanks = new HashMap<>();
+    private Map<Long, Long> notInUseTanks = new HashMap<>();
+    private Map<Long, Long> inUseTanks = new HashMap<>();
+
+    public Inventory(Map<Long, Long> notInUseTanks, Map<Long, Long> inUseTanks) {
+        this.notInUseTanks = notInUseTanks;
+        this.inUseTanks = inUseTanks;
+    }
 
     public Inventory() {
-        oxygenTanks.put(OxygenConstants.Categories.E_ID, 0L);
-        oxygenTanks.put(OxygenConstants.Categories.B_ID, 0L);
-        oxygenTanks.put(OxygenConstants.Categories.A_ID, 0L);
+        notInUseTanks.put(OxygenConstants.Categories.E_ID, 0L);
+        notInUseTanks.put(OxygenConstants.Categories.B_ID, 0L);
+        notInUseTanks.put(OxygenConstants.Categories.A_ID, 0L);
+        inUseTanks.put(OxygenConstants.Categories.E_ID, 0L);
+        inUseTanks.put(OxygenConstants.Categories.B_ID, 0L);
+        inUseTanks.put(OxygenConstants.Categories.A_ID, 0L);
     }
 
-    public Inventory(Map<Long, Long> oxygenTanks) {
-        this.oxygenTanks = oxygenTanks;
+    public void replaceInUseTanks(Long categoryId, Long numberOfTanksToReplace) {
+        inUseTanks.replace(categoryId, numberOfTanksToReplace);
     }
 
-    public void addOxygenTanks(Long categoryId, Long quantity) {
-        oxygenTanks.put(categoryId, oxygenTanks.get(categoryId) + quantity);
+    public void replaceNotInUseTanks(Long categoryId, Long numberOfTanksToReplace) {
+        notInUseTanks.replace(categoryId, numberOfTanksToReplace);
     }
 
-    public Map<Long, Long> getOxygenTanks() {
-        return oxygenTanks;
+    public Long getStoredTanksByCategoryId(Long categoryId) {
+        return notInUseTanks.get(categoryId);
+    }
+
+    public Long getInUseTanksByCategoryId(Long categoryId) {
+        return inUseTanks.get(categoryId);
+    }
+
+    public Map<Long, Long> getNotInUseTanks() {
+        return notInUseTanks;
+    }
+
+    public Map<Long, Long> getInUseTanks() {
+        return inUseTanks;
     }
 }
