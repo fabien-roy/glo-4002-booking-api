937e38eee2746562c373daaa8c46a72c1b81101a
Brian Cloutier <brian.cloutier@hotmail.com>
2019-10-27 11:57:19 -0400

    Added Trip and Passenger

---
+++ booking-api/src/main/java/ca/ulaval/glo4002/booking/domain/shuttles/Passenger.java
@@ -0,0 +1,15 @@
+package ca.ulaval.glo4002.booking.domain.shuttles;
+
+public class Passenger {
+	
+	private Number passNumber;
+	
+	public Passenger(Number passNumber) {
+		this.passNumber = passNumber;
+	}
+	
+	public Number getPassNumber() {
+		return passNumber;
+	}
+
+}
---
+++ booking-api/src/main/java/ca/ulaval/glo4002/booking/domain/shuttles/Trip.java
@@ -0,0 +1,37 @@
+package ca.ulaval.glo4002.booking.domain.shuttles;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import ca.ulaval.glo4002.booking.domain.EventDate;
+
+public class Trip {
+	
+	private EventDate date;
+	private Shuttle shuttle;
+	private List<Passenger> passengers;
+	
+	
+	public Trip(EventDate date, Shuttle shuttle, List<Passenger> passengers) {
+		this.date = date;
+		this.shuttle = shuttle;
+		this.passengers = passengers;
+	}
+	
+	public EventDate getTripDate() {
+		return date;
+	}
+	
+	public String getShuttleName() {
+		return shuttle.getShuttleName();
+	}
+	
+	public List<Number> getPassengersPassNumbers() {
+		List<Number> passNumbers = new ArrayList<>();
+		
+		passengers.stream().map(Passenger::getPassNumber).forEach(number -> passNumbers.add(number));
+		
+		return passNumbers;
+	}
+
+}
