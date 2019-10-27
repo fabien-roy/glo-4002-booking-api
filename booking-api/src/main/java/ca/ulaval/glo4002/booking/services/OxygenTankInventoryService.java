c6d21d0e316abe6114862fe1bcb10de9a0bd7ce7
MonsterX9797 <m-cgo@hotmail.com>
2019-10-27 16:34:43 -0400

    created OxygenTankInventoryService

---
+++ booking-api/src/main/java/ca/ulaval/glo4002/booking/services/OxygenTankInventoryService.java
@@ -0,0 +1,15 @@
+package ca.ulaval.glo4002.booking.services;
+
+import ca.ulaval.glo4002.booking.factories.OxygenTankFactory;
+import ca.ulaval.glo4002.booking.repositories.OxygenTankInventoryRepository;
+
+public class OxygenTankInventoryService {
+	private final OxygenTankInventoryRepository repository;
+	private final OxygenTankFactory factory;
+
+	public OxygenTankInventoryService(OxygenTankInventoryRepository repository, OxygenTankFactory factory) {
+		this.repository = repository;
+		this.factory = factory;
+	}
+
+}
