603ec64429877ee7954069b761ee09cc276b097e
Brian Cloutier <brian.cloutier@hotmail.com>
2019-10-29 09:56:01 -0400

    Added pass category to passenger

--- booking-api/src/main/java/ca/ulaval/glo4002/booking/domain/trip/Passenger.java
+++ booking-api/src/main/java/ca/ulaval/glo4002/booking/domain/trip/Passenger.java
@@ -1,16 +1,23 @@
 package ca.ulaval.glo4002.booking.domain.trip;
 
 import ca.ulaval.glo4002.booking.domain.Number;
+import ca.ulaval.glo4002.booking.enums.PassCategories;
 
 public class Passenger {
 	
 	private Number passNumber;
+	private PassCategories passCategory;
 	
-	public Passenger(Number passNumber) {
+	public Passenger(Number passNumber, PassCategories passCategory) {
 		this.passNumber = passNumber;
+		this.passCategory = passCategory;
 	}
 	
 	public Number getPassNumber() {
 		return passNumber;
+	}
+	
+	public PassCategories getPassCategory() {
+		return passCategory;
 	}
 }
