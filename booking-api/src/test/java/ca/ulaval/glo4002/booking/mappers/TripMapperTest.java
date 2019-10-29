9ad6772fa91b136f97dcee156053a7115c9623a9
Brian Cloutier <brian.cloutier@hotmail.com>
2019-10-28 21:42:29 -0400

    Added correct assert import

--- booking-api/src/test/java/ca/ulaval/glo4002/booking/mappers/TripMapperTest.java
+++ booking-api/src/test/java/ca/ulaval/glo4002/booking/mappers/TripMapperTest.java
@@ -1,6 +1,6 @@
 package ca.ulaval.glo4002.booking.mappers;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
