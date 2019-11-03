fc8981ca839e636300bb2a3aa011258bf7956fe4
Fabien Roy <fabienroy28@gmail.com>
2019-11-02 22:28:43 -0400

    Add PassList price test

---
+++ booking-api/src/test/java/ca/ulaval/glo4002/booking/domain/passes/PassListTest.java
@@ -0,0 +1,36 @@
+package ca.ulaval.glo4002.booking.domain.passes;
+
+import ca.ulaval.glo4002.booking.domain.money.Money;
+import ca.ulaval.glo4002.booking.enums.PassCategories;
+import ca.ulaval.glo4002.booking.enums.PassOptions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.math.BigDecimal;
+import java.util.Collections;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+class PassListTest {
+
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2, 3})
+    public void constructing_shouldSetCorrectPrice(int passQuantity) {
+        Money passPrice = new Money(new BigDecimal(100.0));
+        Money expectedPrice = passPrice.multiply(new BigDecimal(passQuantity));
+        Pass pass = mock(Pass.class);
+        when(pass.getPrice()).thenReturn(passPrice);
+        List<Pass> passes = Collections.nCopies(passQuantity, pass);
+
+        PassList passList = new PassList(
+                passes,
+                new PassCategory(PassCategories.SUPERNOVA.toString(), null),
+                new PassOption(PassOptions.PACKAGE.toString())
+        );
+
+        assertEquals(expectedPrice, passList.getPrice());
+    }
+}
