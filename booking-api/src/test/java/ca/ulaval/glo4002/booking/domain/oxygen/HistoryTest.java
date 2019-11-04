25168d3e56fe5c5cee1ed10ecff3c5dce562ceda
MonsterX9797 <m-cgo@hotmail.com>
2019-11-04 15:38:23 -0500

    domain history test setup

--- booking-api/src/test/java/ca/ulaval/glo4002/booking/domain/oxygen/HistoryTest.java
+++ booking-api/src/test/java/ca/ulaval/glo4002/booking/domain/oxygen/HistoryTest.java
@@ -1,6 +1,12 @@
 package ca.ulaval.glo4002.booking.domain.oxygen;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+import java.time.LocalDate;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -8,9 +14,32 @@ import org.junit.jupiter.api.Test;
 class HistoryTest {
 
 	private History history;
+	private List<OxygenTank> aOxygenTankList;
+	private List<OxygenTank> anOtherOxygenTankList;
+	private List<OxygenTank> aThirdOxygenTankList;
+	private LocalDate aRequestDate = LocalDate.of(2050, 7, 1);
+	private LocalDate anOtherRequestDate = LocalDate.of(2050, 6, 22);
+	private LocalDate aProducedDate = LocalDate.of(2050, 7, 10);
 
 	@BeforeEach
 	void setupHistory() {
+		setUpOxygenList();
+		Map<LocalDate, List<OxygenTank>> requestedOxygenTanks = new HashMap<LocalDate, List<OxygenTank>>();
+		Map<LocalDate, List<OxygenTank>> producedOxygenTanks = new HashMap<LocalDate, List<OxygenTank>>();
+		requestedOxygenTanks.put(aRequestDate, aOxygenTankList);
+		requestedOxygenTanks.put(anOtherRequestDate, anOtherOxygenTankList);
+		producedOxygenTanks.put(aProducedDate, aThirdOxygenTankList);
+		history = new History(requestedOxygenTanks, producedOxygenTanks);
+	}
+
+	private void setUpOxygenList() {
+		OxygenTank tank = mock(OxygenTank.class);
+		aOxygenTankList.add(tank);
+		aOxygenTankList.add(tank);
+		aOxygenTankList.add(tank);
+		anOtherOxygenTankList.add(tank);
+		anOtherOxygenTankList.add(tank);
+		aThirdOxygenTankList.add(tank);
 	}
 
 	@Test
