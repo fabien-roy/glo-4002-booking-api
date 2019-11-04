c91fd72765f75d5b635c0a254ac6b5ce5e08f00e
MonsterX9797 <m-cgo@hotmail.com>
2019-11-04 16:11:16 -0500

    domain report class and test finished

--- booking-api/src/test/java/ca/ulaval/glo4002/booking/domain/oxygen/ReportTest.java
+++ booking-api/src/test/java/ca/ulaval/glo4002/booking/domain/oxygen/ReportTest.java
@@ -1,33 +1,43 @@
 package ca.ulaval.glo4002.booking.domain.oxygen;
 
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class ReportTest {
 
 	Report report;
+	History mockHistory;
+	OxygenTankInventory mockOxygenTankInventory;
 
 	@BeforeEach
 	void setUpReport() {
+		mockHistory = mock(History.class);
+		mockOxygenTankInventory = mock(OxygenTankInventory.class);
+		report = new Report(mockHistory, mockOxygenTankInventory);
 	}
 
 	@Test
 	void constructing_shouldSetHistory() {
-
+		report = new Report();
+		assertTrue(report.getHistory() instanceof History);
 	}
 
 	@Test
 	void constructing_shouldSetOxygenTankInventory() {
-
+		report = new Report();
+		assertTrue(report.getOxygenTankInventory() instanceof OxygenTankInventory);
 	}
 
 	@Test
 	void getHistory_shouldReturnHistory() {
-
+		assertEquals(report.getHistory(), mockHistory);
 	}
 
 	@Test
 	void getOxygenTankInventory_shouldReturnOxygenTankInventory() {
-
+		assertEquals(report.getOxygenTankInventory(), mockOxygenTankInventory);
 	}
 }
