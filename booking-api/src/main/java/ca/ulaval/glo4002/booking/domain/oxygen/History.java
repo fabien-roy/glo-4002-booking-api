0bfed38a924edfc309c9f841ab77e7dc0a350ed4
MonsterX9797 <m-cgo@hotmail.com>
2019-11-04 14:53:12 -0500

    domain class/test history&report empty creation

---
+++ booking-api/src/main/java/ca/ulaval/glo4002/booking/domain/oxygen/History.java
@@ -0,0 +1,5 @@
+package ca.ulaval.glo4002.booking.domain.oxygen;
+
+public class History {
+
+}
---
+++ booking-api/src/test/java/ca/ulaval/glo4002/booking/domain/oxygen/ReportTest.java
@@ -0,0 +1,14 @@
+package ca.ulaval.glo4002.booking.domain.oxygen;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class ReportTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
