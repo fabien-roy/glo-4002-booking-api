12748e6bf180bf72d9a1187161d4d74ed0630579
Fabien Roy <fabienroy28@gmail.com>
2019-11-05 13:32:43 -0500

    Add empty tests for EventFactory

--- booking-api/src/test/java/ca/ulaval/glo4002/booking/factories/EventFactoryTest.java
+++ booking-api/src/test/java/ca/ulaval/glo4002/booking/factories/EventFactoryTest.java
@@ -1,6 +1,84 @@
 package ca.ulaval.glo4002.booking.factories;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 class EventFactoryTest {
 
-    // TODO
+    private EventFactory eventFactory;
+
+    @BeforeEach
+    void setUpFactory() {
+        this.eventFactory = new EventFactory();
+    }
+
+    @Test
+    void build_shouldBuildCorrectAmountOfEvents() {
+        // TODO
+    }
+
+    @Test
+    void build_shouldBuildCorrectEventDate() {
+        // TODO
+    }
+
+    @Test
+    void build_shouldBuildCorrectEventDates_whenThereAreMultipleEvents() {
+        // TODO
+    }
+
+    @Test
+    void build_shouldBuildCorrectActivity() {
+        // TODO
+    }
+
+    @Test
+    void build_shouldBuildCorrectActivities_whenThereAreMultipleEvents() {
+        // TODO
+    }
+
+    @Test
+    void build_shouldBuildCorrectArtist() {
+        // TODO
+    }
+
+    @Test
+    void build_shouldBuildCorrectArtists_whenThereAreMultipleEvents() {
+        // TODO
+    }
+
+    @Test
+    void build_shouldThrowInvalidProgramException_whenEventDateIsDuplicate() {
+        // TODO
+    }
+
+    @Test
+    void build_shouldThrowInvalidProgramException_whenEventDateIsAbsent() {
+        // TODO
+    }
+
+    @Test
+    void build_shouldThrowInvalidProgramException_whenPmIsDefinedTwice() {
+        // TODO
+    }
+
+    @Test
+    void build_shouldThrowInvalidProgramException_whenAmIsDefinedTwice() {
+        // TODO
+    }
+
+    @Test
+    void build_shouldThrowInvalidProgramException_whenAmIsArtist() {
+        // TODO
+    }
+
+    @Test
+    void build_shouldThrowInvalidProgramException_whenPmIsActivity() {
+        // TODO
+    }
+
+    @Test
+    void build_shouldThrowInvalidProgramException_whenArtistIsDuplicate() {
+        // TODO
+    }
 }
