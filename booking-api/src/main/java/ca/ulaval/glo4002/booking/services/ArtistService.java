b38301a2fb08adef67f4f3563818acfdc44f3999
Fabien Roy <fabienroy28@gmail.com>
2019-11-05 17:22:39 -0500

    Add get artists by null

--- booking-api/src/main/java/ca/ulaval/glo4002/booking/services/ArtistService.java
+++ booking-api/src/main/java/ca/ulaval/glo4002/booking/services/ArtistService.java
@@ -1,24 +1,36 @@
 package ca.ulaval.glo4002.booking.services;
 
+import ca.ulaval.glo4002.booking.domain.Artist;
 import ca.ulaval.glo4002.booking.dto.events.ArtistListDto;
+import ca.ulaval.glo4002.booking.repositories.ArtistRepository;
 import ca.ulaval.glo4002.booking.repositories.EventRepository;
 
 import javax.inject.Inject;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class ArtistService {
 
+    private final ArtistRepository artistRepository;
     private final EventRepository eventRepository;
 
     @Inject
-    public ArtistService(EventRepository eventRepository) {
+    public ArtistService(ArtistRepository artistRepository, EventRepository eventRepository) {
+        this.artistRepository = artistRepository;
         this.eventRepository = eventRepository;
     }
 
     public ArtistListDto getAll(String orderBy) {
-        // TODO : With orderBy null
+        List<String> artistNames = new ArrayList<>();
+
+        if (orderBy == null) {
+            artistNames = artistRepository.getAll().stream().map(Artist::getName).collect(Collectors.toList());
+        }
+
         // TODO : With orderBy lowCost
         // TODO : With orderBy mostPopular
 
-        return null;
+        return new ArtistListDto(artistNames);
     }
 }
--- booking-api/src/test/java/ca/ulaval/glo4002/booking/services/ArtistServiceTest.java
+++ booking-api/src/test/java/ca/ulaval/glo4002/booking/services/ArtistServiceTest.java
@@ -1,6 +1,60 @@
 package ca.ulaval.glo4002.booking.services;
 
+import ca.ulaval.glo4002.booking.domain.Artist;
+import ca.ulaval.glo4002.booking.dto.events.ArtistListDto;
+import ca.ulaval.glo4002.booking.repositories.ArtistRepository;
+import ca.ulaval.glo4002.booking.repositories.EventRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.Collections;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
 class ArtistServiceTest {
 
-    // TODO
+    ArtistService service;
+    ArtistRepository artistRepository;
+
+    @BeforeEach
+    void setUpService() {
+        artistRepository = mock(ArtistRepository.class);
+        EventRepository eventRepository = mock(EventRepository.class);
+
+        service = new ArtistService(artistRepository, eventRepository);
+    }
+
+    @Test
+    void getAll_shouldReturnAllArtistNames_whenOrderByIsNull() {
+        String expectedArtistName = "expectedArtistName";
+        Artist artist = mock(Artist.class);
+        when(artist.getName()).thenReturn(expectedArtistName);
+        when(artistRepository.getAll()).thenReturn(Collections.singletonList(artist));
+
+        ArtistListDto artistListDto = service.getAll(null);
+
+        assertEquals(expectedArtistName, artistListDto.getArtists().get(0));
+    }
+
+    @Test
+    void getAll_shouldReturnAllArtistNamesOrderedByPopularity_whenOrderByIsNull() {
+        // TODO
+    }
+
+    @Test
+    void getAll_shouldReturnAllArtistNamesOrderedByPopularityAndByCost_whenOrderByIsNull() {
+        // TODO
+    }
+
+    @Test
+    void getAll_shouldReturnAllArtistNamesOrderedByCost_whenOrderByIsByCost() {
+        // TODO
+    }
+
+    @Test
+    void getAll_shouldReturnAllArtistNamesOrderedByPopularity_whenOrderByIsByPopularity() {
+        // TODO
+    }
 }
