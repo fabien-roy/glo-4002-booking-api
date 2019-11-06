437c8d8320f3c66fc63b809e24fa9fe6996748a7
Brian Cloutier <brian.cloutier@hotmail.com>
2019-11-06 17:19:47 -0500

    Renamed Artist to BookingArtist

--- booking-api/src/main/java/ca/ulaval/glo4002/booking/domain/BookingArtist.java
+++ booking-api/src/main/java/ca/ulaval/glo4002/booking/domain/BookingArtist.java
@@ -2,43 +2,57 @@ package ca.ulaval.glo4002.booking.domain;
 
 import ca.ulaval.glo4002.booking.domain.money.Money;
 
-public class Artist {
+public class BookingArtist {
 
     private String name;
     private Money cost;
-    private Integer membersAmount;
-
-    public Artist(String name, Money cost, Integer membersAmount) {
-        this.name = name;
-        this.cost = cost;
-        this.membersAmount = membersAmount;
-    }
-
+    private Integer numberOfPeople;
+    private String musicStyle;
+    private Integer popularityRank;
+    
+    
+    public BookingArtist(String name, Money cost, Integer numberOfPeople,
+    		String musicStyle, Integer popularityRank) {
+		this.name = name;
+		this.cost = cost;
+		this.numberOfPeople = numberOfPeople;
+		this.musicStyle = musicStyle;
+		this.popularityRank = popularityRank;
+	}
+    
     public String getName() {
-        return name;
-    }
+		return name;
+	}
 
-    public Money getCost() {
-        return cost;
-    }
+	public Money getCost() {
+		return cost;
+	}
 
-    public Integer getMembersAmount() {
-        return membersAmount;
-    }
+	public Integer getNumberOfPeople() {
+		return numberOfPeople;
+	}
 
-    @Override
+	public String getMusicStyle() {
+		return musicStyle;
+	}
+
+	public Integer getPopularityRank() {
+		return popularityRank;
+	}
+
+	@Override
     public boolean equals(Object other) {
-        if (!(other instanceof Artist)) return false;
+        if (!(other instanceof BookingArtist)) return false;
 
-        Artist otherArtist = (Artist) other;
+        BookingArtist otherArtist = (BookingArtist) other;
 
         return name.equals(otherArtist.getName())
                 && cost.equals(otherArtist.getCost())
-                && membersAmount.equals(otherArtist.getMembersAmount());
+                && numberOfPeople.equals(otherArtist.getNumberOfPeople());
     }
 
     @Override
     public int hashCode() {
-        return name.hashCode() + cost.hashCode() + membersAmount.hashCode();
+        return name.hashCode() + cost.hashCode() + numberOfPeople.hashCode();
     }
 }
