c90b0e52521d56e84f91712f28c36026debb0ed1
MonsterX9797 <m-cgo@hotmail.com>
2019-11-08 11:06:32 -0500

    empty reportMapper class finish, test not done

--- booking-api/src/main/java/ca/ulaval/glo4002/booking/mappers/OxygenReportMapper.java
+++ booking-api/src/main/java/ca/ulaval/glo4002/booking/mappers/OxygenReportMapper.java
@@ -1,10 +1,18 @@
 package ca.ulaval.glo4002.booking.mappers;
 
+import java.util.List;
+
 import ca.ulaval.glo4002.booking.domain.oxygen.Report;
+import ca.ulaval.glo4002.booking.dto.oxygen.HistoryItemDto;
 import ca.ulaval.glo4002.booking.dto.oxygen.OxygenReportDto;
+import ca.ulaval.glo4002.booking.dto.oxygen.OxygenTankInventoryDto;
 
 public class OxygenReportMapper {
 	public OxygenReportDto toDto(Report report) {
-		return null;
+		OxygenTankInventoryMapper inventoryMapper = new OxygenTankInventoryMapper();
+		List<OxygenTankInventoryDto> inventory = inventoryMapper.toDto(report.getOxygenTankInventory());
+		HistoryItemMapper historyMapper = new HistoryItemMapper();
+		List<HistoryItemDto> history = historyMapper.toDto(report.getHistory());
+		return new OxygenReportDto(inventory, history);
 	}
 }
