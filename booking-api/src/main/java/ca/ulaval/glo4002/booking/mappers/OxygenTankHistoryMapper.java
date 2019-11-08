5e232644bb85c05b76933cea4d14e1d537ce35a8
Fabien Roy <fabienroy28@gmail.com>
2019-11-08 11:38:27 -0500

    Refactor OxygenTankHistoryMapper

--- booking-api/src/main/java/ca/ulaval/glo4002/booking/mappers/OxygenTankHistoryMapper.java
+++ booking-api/src/main/java/ca/ulaval/glo4002/booking/mappers/OxygenTankHistoryMapper.java
@@ -1,48 +1,60 @@
 package ca.ulaval.glo4002.booking.mappers;
 
-import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 
 import ca.ulaval.glo4002.booking.domain.oxygen.History;
 import ca.ulaval.glo4002.booking.domain.oxygen.OxygenTank;
 import ca.ulaval.glo4002.booking.dto.oxygen.HistoryItemDto;
-import ca.ulaval.glo4002.booking.enums.OxygenCategories;
 
 public class OxygenTankHistoryMapper {
 
 	public List<HistoryItemDto> toDto(History history) {
-		List<HistoryItemDto> historyList = new ArrayList<HistoryItemDto>();
+		List<HistoryItemDto> historyList = new ArrayList<>();
 
-		Map<LocalDate, List<OxygenTank>> requestedOxygenTanks = history.getRequestedOxygenTanks();
-		for (Map.Entry<LocalDate, List<OxygenTank>> entry : requestedOxygenTanks.entrySet()) {
-			LocalDate date = entry.getKey();
-			Integer qtyOxygenTankBought = 0;
-			Integer qtyWaterUsed = 0;
-			Integer qtyCandlesUsed = 0;
-			List<OxygenTank> tanks = entry.getValue();
-			Integer qtyOxygenTankMade = tanks.size();
-			for (int i = 0; i < tanks.size(); i++) {
-				if (tanks.get(i).getCategory() == OxygenCategories.E) {
-					int nbRessource = OxygenTank.CATEGORY_E_NUMBER_OF_RESOURCES_NEEDED;
-					int nbTank = OxygenTank.CATEGORY_E_NUMBER_OF_TANKS_CREATED;
-					qtyOxygenTankBought += nbRessource / nbTank;
-				} else if (tanks.get(i).getCategory() == OxygenCategories.B) {
-					int nbRessource = OxygenTank.CATEGORY_B_NUMBER_OF_RESOURCES_NEEDED;
-					int nbTank = OxygenTank.CATEGORY_B_NUMBER_OF_TANKS_CREATED;
-					qtyWaterUsed += nbRessource / nbTank;
-				} else if (tanks.get(i).getCategory() == OxygenCategories.A) {
-					int nbRessource = OxygenTank.CATEGORY_A_NUMBER_OF_RESOURCES_NEEDED;
-					int nbTank = OxygenTank.CATEGORY_A_NUMBER_OF_TANKS_CREATED;
-					qtyCandlesUsed += nbRessource / nbTank;
+		history.getRequestedOxygenTanks().forEach((requestedDate, oxygenTanks) -> {
+			Long qtyOxygenTankBought = 0L;
+			Long qtyWaterUsed = 0L;
+			Long qtyCandlesUsed = 0L;
+			Integer qtyOxygenTankMade = oxygenTanks.size();
+
+			for(OxygenTank oxygenTank : oxygenTanks) {
+			    Integer nbResource;
+			    Integer nbTank;
+
+				switch(oxygenTank.getCategory()) {
+					case E:
+						nbResource = OxygenTank.CATEGORY_E_NUMBER_OF_RESOURCES_NEEDED;
+						nbTank = OxygenTank.CATEGORY_E_NUMBER_OF_TANKS_CREATED;
+
+						qtyOxygenTankBought += nbResource / nbTank;
+						break;
+					case B:
+						nbResource = OxygenTank.CATEGORY_B_NUMBER_OF_RESOURCES_NEEDED;
+						nbTank = OxygenTank.CATEGORY_B_NUMBER_OF_TANKS_CREATED;
+
+						qtyWaterUsed += nbResource / nbTank;
+						break;
+					default:
+					case A:
+						nbResource = OxygenTank.CATEGORY_A_NUMBER_OF_RESOURCES_NEEDED;
+						nbTank = OxygenTank.CATEGORY_A_NUMBER_OF_TANKS_CREATED;
+
+						qtyCandlesUsed += nbResource / nbTank;
+						break;
 				}
 			}
 
-			HistoryItemDto dto = new HistoryItemDto(date.toString(), qtyOxygenTankBought.longValue(),
-					qtyWaterUsed.longValue(), qtyCandlesUsed.longValue(), qtyOxygenTankMade.longValue());
+			HistoryItemDto dto = new HistoryItemDto(
+					requestedDate.toString(),
+					qtyOxygenTankBought,
+					qtyWaterUsed,
+					qtyCandlesUsed,
+					qtyOxygenTankMade.longValue()
+			);
+
 			historyList.add(dto);
-		}
+		});
 
 		return historyList;
 	}
