7a1f9b8cb6614d47207ce627f415316aea8007c6
Fabien Roy <fabienroy28@gmail.com>
2019-11-07 22:56:13 -0500

    Refactor ArtistService

--- booking-api/src/main/java/ca/ulaval/glo4002/booking/services/ArtistService.java
+++ booking-api/src/main/java/ca/ulaval/glo4002/booking/services/ArtistService.java
@@ -1,13 +1,11 @@
 package ca.ulaval.glo4002.booking.services;
 
-import ca.ulaval.glo4002.booking.domain.BookingArtist;
 import ca.ulaval.glo4002.booking.dto.events.ArtistListDto;
 import ca.ulaval.glo4002.booking.enums.ArtistOrderings;
 import ca.ulaval.glo4002.organisation.domain.Artist;
 import ca.ulaval.glo4002.organisation.repositories.ArtistRepository;
 
 import javax.inject.Inject;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -20,19 +18,37 @@ public class ArtistService {
         this.artistRepository = artistRepository;
     }
 
-    // TODO : ArtistService.getAll()
     public ArtistListDto getAll(String orderBy) {
         List<Artist> artists = artistRepository.findAll();
-        List<String> artistNames = new ArrayList<>();
 
-        if (orderBy == null) {
-            artistNames = artists.stream().map(Artist::getName).collect(Collectors.toList());
-        } else {
+        if (orderBy != null) {
             ArtistOrderings ordering = ArtistOrderings.get(orderBy);
 
-            // TODO : Order
+            switch (ordering) {
+                case LOW_COSTS:
+                    artists = orderByLowCost(artists);
+                    break;
+                default:
+                case MOST_POPULAR:
+                    artists = orderByMostPopular(artists);
+                    break;
+            }
         }
 
+        List<String> artistNames = getArtistNames(artists);
+
         return new ArtistListDto(artistNames);
+    }
+
+    private List<String> getArtistNames(List<Artist> artists) {
+        return artists.stream().map(Artist::getName).collect(Collectors.toList());
+    }
+
+    private List<Artist> orderByMostPopular(List<Artist> artists) {
+        return artists;
+    }
+
+    private List<Artist> orderByLowCost(List<Artist> artists) {
+        return artists;
     }
 }
