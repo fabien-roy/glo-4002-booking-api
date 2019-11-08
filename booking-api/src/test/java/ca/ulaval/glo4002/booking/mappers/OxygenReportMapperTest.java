8d734597030704e860ddf934129f8cc2c14f28d0
MonsterX9797 <m-cgo@hotmail.com>
2019-11-08 11:02:17 -0500

    empty reportMapper setupTEsts

--- booking-api/src/test/java/ca/ulaval/glo4002/booking/mappers/OxygenReportMapperTest.java
+++ booking-api/src/test/java/ca/ulaval/glo4002/booking/mappers/OxygenReportMapperTest.java
@@ -1,25 +1,42 @@
 package ca.ulaval.glo4002.booking.mappers;
 
+import static org.mockito.Mockito.*;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import ca.ulaval.glo4002.booking.domain.oxygen.History;
+import ca.ulaval.glo4002.booking.domain.oxygen.OxygenTankInventory;
+import ca.ulaval.glo4002.booking.domain.oxygen.Report;
+import ca.ulaval.glo4002.booking.dto.oxygen.OxygenReportDto;
 
 class OxygenReportMapperTest {
 
 	OxygenReportMapper mapper;
+	Report mockedReport;
 
 	@BeforeEach
 	void setUpMapper() {
 		mapper = new OxygenReportMapper();
 	}
 
+	@BeforeEach
+	void setUpReport() {
+		mockedReport = mock(Report.class);
+		History mockedHistory = mock(History.class);
+		OxygenTankInventory mockedOxygenTankInventory = mock(OxygenTankInventory.class);
+		when(mockedReport.getHistory()).thenReturn(mockedHistory);
+		when(mockedReport.getOxygenTankInventory()).thenReturn(mockedOxygenTankInventory);
+	}
+
 	@Test
 	void dto_shouldBuildWithCorrectInventoryDtoList() {
-
+		OxygenReportDto reportDto = mapper.toDto(mockedReport);
 	}
 
 	@Test
 	void dto_shouldBuildWithCorrectHistoryDtoList() {
-
+		OxygenReportDto reportDto = mapper.toDto(mockedReport);
 	}
 
 }
