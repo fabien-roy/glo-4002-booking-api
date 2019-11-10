2e656f198a0aadb2e21c0063236c6ca61954cf9f
Guillaume Begin <guillaume.begin.3@ulaval.ca>
2019-11-10 00:08:46 -0500

    changed OxygenInventoryServiceTest, corrected method that was verifying service

--- booking-api/src/test/java/ca/ulaval/glo4002/booking/services/OxygenInventoryServiceTest.java
+++ booking-api/src/test/java/ca/ulaval/glo4002/booking/services/OxygenInventoryServiceTest.java
@@ -1,40 +1,41 @@
 package ca.ulaval.glo4002.booking.services;
 
+import ca.ulaval.glo4002.booking.domain.oxygen.OxygenTanksProducer;
+import ca.ulaval.glo4002.booking.enums.OxygenCategories;
+import ca.ulaval.glo4002.booking.mappers.OxygenInventoryMapper;
+import ca.ulaval.glo4002.booking.repositories.OxygenTankInventoryRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 
-import java.time.LocalDate;
-
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-
-import ca.ulaval.glo4002.booking.enums.OxygenCategories;
-import ca.ulaval.glo4002.booking.factories.OxygenTankFactory;
-import ca.ulaval.glo4002.booking.mappers.OxygenInventoryMapper;
-import ca.ulaval.glo4002.booking.repositories.OxygenTankInventoryRepository;
-
 class OxygenInventoryServiceTest {
 
 	private OxygenTankInventoryService oxygenTankInventoryService;
-	private OxygenTankInventoryRepository repository;
+	private OxygenTanksProducer producer;
+	private Integer numberOfDays;
 
 	@BeforeEach
 	void setUpOxygenTankInventoryService() {
-		repository = mock(OxygenTankInventoryRepository.class);
-		OxygenTankFactory factory = mock(OxygenTankFactory.class);
+		OxygenTankInventoryRepository repository = mock(OxygenTankInventoryRepository.class);
+		producer = mock(OxygenTanksProducer.class);
 		OxygenInventoryMapper mapper = mock(OxygenInventoryMapper.class);
 
-		oxygenTankInventoryService = new OxygenTankInventoryService(repository, factory, mapper);
+		numberOfDays = 1;
+		oxygenTankInventoryService = new OxygenTankInventoryService(repository, producer, mapper);
 	}
 
+	// TODO : OXY : not sure just changed so travis build is not in error 
 	@Test
 	void orderOxygenTank_shouldOrderOxygenTanks() {
 		OxygenCategories category = OxygenCategories.A;
 		LocalDate requestDate = LocalDate.of(2050, 06, 17);
-		Integer numberOfDays = 1;
 		oxygenTankInventoryService.orderOxygenTanks(category, requestDate, numberOfDays);
-		// TODO not sure about this test, should maybe refactor
-		verify(repository).setInventory(any());
+
+		verify(producer).produceOxygenForOrder(any(), any(), any());
 	}
 }
