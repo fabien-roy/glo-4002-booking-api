deb3a8e88bbd7ca7b2641e364ae0aac182443811
Brian Cloutier <brian.cloutier@hotmail.com>
2019-11-12 17:24:58 -0500

    Artist client test

---
+++ booking-api/src/test/java/ca/ulaval/glo4002/booking/clients/ArtistClientTest.java
@@ -0,0 +1,28 @@
+package ca.ulaval.glo4002.booking.clients;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
+import java.util.List;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import ca.ulaval.glo4002.booking.domain.artist.ExternalArtist;
+
+public class ArtistClientTest {
+	
+	private static ArtistClient artistClient;
+	
+	@BeforeAll
+	public static void setupClient() {
+		artistClient = new ArtistClient();
+	}
+	
+	@Test
+	public void getArtists_returnsAllArtists() {
+		List<ExternalArtist> externalArtists = artistClient.getArtists();
+		
+		assertFalse(externalArtists.isEmpty());
+	}
+
+}
