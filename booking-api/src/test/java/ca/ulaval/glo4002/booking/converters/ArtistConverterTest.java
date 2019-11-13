c90202d30030393c394c2edf65158537a92a2218
Brian Cloutier <brian.cloutier@hotmail.com>
2019-11-13 18:24:52 -0500

    Artist converter test

--- booking-api/src/test/java/ca/ulaval/glo4002/booking/converters/ArtistConverterTest.java
+++ booking-api/src/test/java/ca/ulaval/glo4002/booking/converters/ArtistConverterTest.java
@@ -1,22 +1,17 @@
 package ca.ulaval.glo4002.booking.converters;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
-import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.junit.Ignore;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 
-import ca.ulaval.glo4002.booking.domain.artist.BookingArtist;
 import ca.ulaval.glo4002.booking.domain.artist.ExternalArtist;
-import ca.ulaval.glo4002.booking.domain.artist.ExternalArtistAvailability;
-import ca.ulaval.glo4002.booking.domain.events.EventDate;
 import ca.ulaval.glo4002.booking.repositories.ArtistRepository;
 
 public class ArtistConverterTest {
@@ -45,37 +40,10 @@ public class ArtistConverterTest {
 	}
 	
 	@Test
-	@Ignore("WIP")
 	public void convert_convertedArtistsShouldAllBeConverted() {
 		artistConverter.convert(externalArtists);
-
-	}
-	
-	@Test
-	@Ignore("WIP")
-	public void convert_convertedArtistsShouldHaveRightName() {
-		artistConverter.convert(externalArtists);
-
-	}
-	
-	@Test
-	@Ignore("WIP")
-	public void convert_convertedArtistsShouldHaveNoAvailabilitiesWhenExternalHasNone() {
-		artistConverter.convert(externalArtists);
-
-	}
-	
-	@Test
-	@Ignore("WIP")
-	public void convert_convertedArtistsShouldHaveAvailabilitiesWhenExternalHasSome() {
-		when(anExternalArtist.getAvailabilities()).thenReturn(new ArrayList<>());
-		ExternalArtistAvailability anExternalArtistAvailability = mock(ExternalArtistAvailability.class);
-		when(anExternalArtistAvailability.getDate()).thenReturn("2050-07-19");
-		EventDate anExternalDate = new EventDate(LocalDate.parse("2050-07-19"));
 		
-		anExternalArtist.getAvailabilities().add(anExternalArtistAvailability);
-		
-		artistConverter.convert(externalArtists);
+		Mockito.verify(artistRepository).saveAll(Mockito.anyList());
 
 	}
 
