cc12f6b13790bac3eb8a51657b48fbff634b2996
Brian Cloutier <brian.cloutier@hotmail.com>
2019-11-13 18:39:05 -0500

    More converter tests

--- booking-api/src/test/java/ca/ulaval/glo4002/booking/converters/ArtistConverterTest.java
+++ booking-api/src/test/java/ca/ulaval/glo4002/booking/converters/ArtistConverterTest.java
@@ -1,5 +1,8 @@
 package ca.ulaval.glo4002.booking.converters;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
@@ -9,10 +12,13 @@ import java.util.List;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
 
+import ca.ulaval.glo4002.booking.domain.artist.BookingArtist;
 import ca.ulaval.glo4002.booking.domain.artist.ExternalArtist;
+import ca.ulaval.glo4002.booking.domain.artist.ExternalArtistAvailability;
+import ca.ulaval.glo4002.booking.domain.events.EventDate;
 import ca.ulaval.glo4002.booking.repositories.ArtistRepository;
+import ca.ulaval.glo4002.booking.repositories.InMemoryArtistRepository;
 
 public class ArtistConverterTest {
 	
@@ -35,7 +41,7 @@ public class ArtistConverterTest {
 	
 	@BeforeEach
 	public void converterSetUp() {
-		artistRepository = mock(ArtistRepository.class);
+		artistRepository = new InMemoryArtistRepository();
 		artistConverter = new ArtistConverter(artistRepository);
 	}
 	
@@ -43,8 +49,54 @@ public class ArtistConverterTest {
 	public void convert_convertedArtistsShouldAllBeConverted() {
 		artistConverter.convert(externalArtists);
 		
-		Mockito.verify(artistRepository).saveAll(Mockito.anyList());
+		List<BookingArtist> artists = artistRepository.findAll();
+		
+		assertEquals(externalArtists.size(), artists.size());
 
 	}
+	
+	@Test
+	public void convert_convertArtistsShouldHaveRightNames() {
+		artistConverter.convert(externalArtists);
+		
+		List<BookingArtist> artists = artistRepository.findAll();
+		
+		assertEquals(externalArtists.get(0).getName(), artists.get(0).getName());
+		assertEquals(externalArtists.get(1).getName(), artists.get(1).getName());
+	}
+	
+	@Test
+	public void convert_convertArtistsShouldHaveAvailabilities_whenExternalHasSome() {
+		when(anExternalArtist.getAvailabilities()).thenReturn(new ArrayList<>());
+		ExternalArtistAvailability externalArtistAvailability = mock(ExternalArtistAvailability.class);
+		when(externalArtistAvailability.getDate()).thenReturn(EventDate.START_DATE.toString());
+		externalArtists.get(0).getAvailabilities().add(externalArtistAvailability);
+		
+		artistConverter.convert(externalArtists);
+		
+		List<BookingArtist> artists = artistRepository.findAll();
+		
+		assertFalse(artists.get(0).getAvailabilities().isEmpty());
+		assertEquals(externalArtists.get(0).getAvailabilities().size(),
+				artists.get(0).getAvailabilities().size());
+		
+	}
+	
+	@Test
+	public void convert_convertArtistsShouldNotHaveAvailabilities_whenExternalHasSomeOutsideFestival() {
+		when(anExternalArtist.getAvailabilities()).thenReturn(new ArrayList<>());
+		ExternalArtistAvailability externalArtistAvailability = mock(ExternalArtistAvailability.class);
+		when(externalArtistAvailability.getDate()).thenReturn(EventDate.START_DATE.minusDays(1).toString());
+		externalArtists.get(0).getAvailabilities().add(externalArtistAvailability);
+		
+		artistConverter.convert(externalArtists);
+		
+		List<BookingArtist> artists = artistRepository.findAll();
+		
+		assertTrue(artists.get(0).getAvailabilities().isEmpty());
+		
+	}
+	
+	
 
 }
