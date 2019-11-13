b0c4379d3610d5107fa38d763f4718c3b88b4b4a
Fabien Roy <fabienroy28@gmail.com>
2019-11-12 21:53:23 -0500

    Write test names for oxygen inventory service

--- booking-api/src/test/java/ca/ulaval/glo4002/booking/services/OxygenInventoryServiceTest.java
+++ booking-api/src/test/java/ca/ulaval/glo4002/booking/services/OxygenInventoryServiceTest.java
@@ -1,5 +1,6 @@
 package ca.ulaval.glo4002.booking.services;
 
+import ca.ulaval.glo4002.booking.enums.PassCategories;
 import ca.ulaval.glo4002.booking.factories.OxygenFactory;
 import ca.ulaval.glo4002.booking.producers.OxygenTankProducer;
 import ca.ulaval.glo4002.booking.enums.OxygenCategories;
@@ -7,6 +8,8 @@ import ca.ulaval.glo4002.booking.mappers.OxygenInventoryMapper;
 import ca.ulaval.glo4002.booking.repositories.OxygenInventoryRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.EnumSource;
 
 import java.time.LocalDate;
 
@@ -16,5 +19,35 @@ import static org.mockito.Mockito.verify;
 
 class OxygenInventoryServiceTest {
 
-    // TODO
+    private OxygenInventoryService service;
+    private OxygenTankProducer producer;
+
+    @BeforeEach
+    void setUpService() {
+        OxygenFactory factory = mock(OxygenFactory.class);
+        producer = mock(OxygenTankProducer.class);
+
+        service = new OxygenInventoryService(factory, producer);
+    }
+
+    @Test
+    void orderForPasses_shouldOrderForFullFestival_whenEventDatesIsNull() {
+        // TODO
+    }
+
+    @Test
+    void orderForPasses_shouldOrderForEventDate() {
+        // TODO
+    }
+
+    @Test
+    void orderForPasses_shouldOrderForEventDates_whenThereAreMultiplePasses() {
+        // TODO
+    }
+
+    @ParameterizedTest
+    @EnumSource(PassCategories.class)
+    void orderForPasses_shouldOrderWithCorrectOxygenCategory(PassCategories passCategory) {
+        // TODO
+    }
 }
