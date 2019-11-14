47f50c8258f13bae913e4c50e00c7c43452ab9cc
MonsterX9797 <m-cgo@hotmail.com>
2019-11-13 19:14:55 -0500

    OrderNumberTest

--- booking-api/src/test/java/ca/ulaval/glo4002/booking/domain/orders/OrderNumberTest.java
+++ booking-api/src/test/java/ca/ulaval/glo4002/booking/domain/orders/OrderNumberTest.java
@@ -1,90 +1,91 @@
 package ca.ulaval.glo4002.booking.domain.orders;
 
-import ca.ulaval.glo4002.booking.domain.Number;
-import ca.ulaval.glo4002.booking.exceptions.InvalidFormatException;
+import static org.junit.jupiter.api.Assertions.*;
+
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import ca.ulaval.glo4002.booking.domain.Number;
+import ca.ulaval.glo4002.booking.exceptions.InvalidFormatException;
 
 class OrderNumberTest {
 
-    private OrderNumber orderNumber;
+	private OrderNumber orderNumber;
 
-    @Test
-    void constructing_shouldThrowInvalidFormatException_whenOrderNumberHasNoSeparator() {
-        String orderNumberWithoutSeparator = "123TEAM";
+	@Test
+	void constructing_shouldThrowInvalidFormatException_whenOrderNumberHasNoSeparator() {
+		String orderNumberWithoutSeparator = "123TEAM";
 
-        assertThrows(InvalidFormatException.class, () -> new OrderNumber(orderNumberWithoutSeparator));
-    }
+		assertThrows(InvalidFormatException.class, () -> new OrderNumber(orderNumberWithoutSeparator));
+	}
 
-    @Test
-    void constructing_shouldThrowInvalidFormatException_whenOrderNumberHasMultipleSeparator() {
-        String orderNumberMultipleSeparators = "123" + OrderNumber.SEPARATOR + "TEAM" + OrderNumber.SEPARATOR + "YEAH";
+	@Test
+	void constructing_shouldThrowInvalidFormatException_whenOrderNumberHasMultipleSeparator() {
+		String orderNumberMultipleSeparators = "123" + OrderNumber.SEPARATOR + "TEAM" + OrderNumber.SEPARATOR + "YEAH";
 
-        assertThrows(InvalidFormatException.class, () -> new OrderNumber(orderNumberMultipleSeparators));
-    }
+		assertThrows(InvalidFormatException.class, () -> new OrderNumber(orderNumberMultipleSeparators));
+	}
 
-    @Test
-    void toString_shouldReturnCorrectFormat() {
-        Number aNumber = new Number(1L);
-        String aVendor = "VENDOR";
-        String expectedResult = aVendor + OrderNumber.SEPARATOR + aNumber.toString();
-        orderNumber = new OrderNumber(aNumber, aVendor);
+	@Test
+	void toString_shouldReturnCorrectFormat() {
+		Number aOrderNumber = new Number(1L);
+		String aVendor = "VENDOR";
+		String expectedOrderNumber = aVendor + OrderNumber.SEPARATOR + aOrderNumber.toString();
 
-        String result = orderNumber.toString();
+		orderNumber = new OrderNumber(aOrderNumber, aVendor);
+		String textOrderNumber = orderNumber.toString();
 
-        assertEquals(expectedResult, result);
-    }
+		assertEquals(expectedOrderNumber, textOrderNumber);
+	}
 
-    @Test
-    void equals_shouldReturnFalse_whenObjectIsNotNumber() {
-        orderNumber = new OrderNumber(new Number(1L), "VENDOR");
-        Object object = new Object();
+	@Test
+	void equals_shouldReturnFalse_whenObjectIsNotNumber() {
+		orderNumber = new OrderNumber(new Number(1L), "VENDOR");
+		Object object = new Object();
 
-        boolean result = orderNumber.equals(object);
+		boolean isSameObject = orderNumber.equals(object);
 
-        assertFalse(result);
-    }
+		assertFalse(isSameObject);
+	}
 
-    @Test
-    void equals_shouldReturnFalse_whenNumberIsNotSameValue() {
-        orderNumber = new OrderNumber(new Number(1L), "VENDOR");
-        OrderNumber otherOrderNumber = new OrderNumber(new Number(2L), "VENDOR");
+	@Test
+	void equals_shouldReturnFalse_whenNumberIsNotSameValue() {
+		orderNumber = new OrderNumber(new Number(1L), "VENDOR");
+		OrderNumber differentOrderNumber = new OrderNumber(new Number(2L), "VENDOR");
 
-        boolean result = orderNumber.equals(otherOrderNumber);
+		boolean isSameOrderNumber = orderNumber.equals(differentOrderNumber);
 
-        assertFalse(result);
-    }
+		assertFalse(isSameOrderNumber);
+	}
 
-    @Test
-    void equals_shouldReturnFalse_whenVendorCodeIsNotSameValue() {
-        orderNumber = new OrderNumber(new Number(1L), "VENDOR");
-        OrderNumber otherOrderNumber = new OrderNumber(new Number(1L), "OTHER_VENDOR");
+	@Test
+	void equals_shouldReturnFalse_whenVendorCodeIsNotSameValue() {
+		orderNumber = new OrderNumber(new Number(1L), "VENDOR");
+		OrderNumber otherOrderNumber = new OrderNumber(new Number(1L), "OTHER_VENDOR");
 
-        boolean result = orderNumber.equals(otherOrderNumber);
+		boolean result = orderNumber.equals(otherOrderNumber);
 
-        assertFalse(result);
-    }
+		assertFalse(result);
+	}
 
-    @Test
-    void equals_shouldReturnFalse_whenSameValue() {
-        orderNumber = new OrderNumber(new Number(1L), "VENDOR");
-        OrderNumber otherOrderNumber = new OrderNumber(new Number(1L), "VENDOR");
+	@Test
+	void equals_shouldReturnFalse_whenSameValue() {
+		orderNumber = new OrderNumber(new Number(1L), "VENDOR");
+		OrderNumber otherOrderNumber = new OrderNumber(new Number(1L), "VENDOR");
 
-        boolean result = orderNumber.equals(otherOrderNumber);
+		boolean result = orderNumber.equals(otherOrderNumber);
 
-        assertTrue(result);
-    }
+		assertTrue(result);
+	}
 
-    @Test
-    void hashCode_shouldReturnNumberPlusVendorCodeHashCode() {
-        Number aNumber = new Number(1L);
-        String aVendorCode = "VENDOR";
-        int expectedHashCode = aNumber.hashCode() + aVendorCode.hashCode();
-        orderNumber = new OrderNumber(aNumber, aVendorCode);
+	@Test
+	void hashCode_shouldReturnNumberPlusVendorCodeHashCode() {
+		Number aNumber = new Number(1L);
+		String aVendorCode = "VENDOR";
+		int expectedHashCode = aNumber.hashCode() + aVendorCode.hashCode();
+		orderNumber = new OrderNumber(aNumber, aVendorCode);
 
-        int hashCode = orderNumber.hashCode();
+		int hashCode = orderNumber.hashCode();
 
-        assertEquals(expectedHashCode, hashCode);
-    }
+		assertEquals(expectedHashCode, hashCode);
+	}
 }
