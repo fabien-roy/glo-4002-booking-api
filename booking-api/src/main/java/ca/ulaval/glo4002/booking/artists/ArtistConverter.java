252febd6797e58b136b7e4a7c0b7a85fdd895aaa
MonsterX9797 <m-cgo@hotmail.com>
2019-11-14 19:01:28 -0500

    clean code artist and add test

--- booking-api/src/main/java/ca/ulaval/glo4002/booking/artists/ArtistConverter.java
+++ booking-api/src/main/java/ca/ulaval/glo4002/booking/artists/ArtistConverter.java
@@ -9,49 +9,47 @@ import ca.ulaval.glo4002.booking.events.EventDate;
 import ca.ulaval.glo4002.booking.profits.Money;
 
 public class ArtistConverter {
-	
+
 	private final ArtistRepository artistRepository;
-	
+
 	public ArtistConverter(ArtistRepository artistRepository) {
 		this.artistRepository = artistRepository;
 	}
-	
+
 	public void convert() {
 		List<ExternalArtist> externalArtists = ArtistClient.getArtists();
 		List<BookingArtist> bookingArtists = new ArrayList<>();
 		for (ExternalArtist externalArtist : externalArtists) {
 			Money cost = new Money(new BigDecimal(externalArtist.getPrice()));
-			
-			
-			bookingArtists.add(new BookingArtist(
-					externalArtist.getId(),
-					externalArtist.getName(),
-					cost,
-					externalArtist.getNbPeople(),
-					externalArtist.getMusicStyle(),
-					externalArtist.getPopularityRank(),
-					convertAvailabilities(externalArtist.getAvailabilities()))
-			);
+
+			List<Availability> convertedAvailabilities = convertAvailabilities(externalArtist.getAvailabilities());
+			BookingArtist bookingArtist = new BookingArtist(externalArtist.getId(), externalArtist.getName(), cost,
+					externalArtist.getNbPeople(), externalArtist.getMusicStyle(), externalArtist.getPopularityRank(),
+					convertedAvailabilities);
+			bookingArtists.add(bookingArtist);
 		}
-		
+
 		artistRepository.saveAll(bookingArtists);
 	}
-	
+
 	private List<Availability> convertAvailabilities(List<ExternalArtistAvailability> externalAvailabilities) {
 		List<Availability> availabilities = new ArrayList<>();
-		for(ExternalArtistAvailability externalArtistAvailability : externalAvailabilities) {
-			LocalDate availabilityDate = LocalDate.parse(externalArtistAvailability.getDate());
-			if(availabilityDateIsDuringFestival(availabilityDate)) {
-				availabilities.add(new Availability
-						(new EventDate(availabilityDate)));
+		for (ExternalArtistAvailability externalArtistAvailability : externalAvailabilities) {
+			String textDate = externalArtistAvailability.getDate();
+			LocalDate availabilityDate = LocalDate.parse(textDate);
+			if (availabilityDateIsDuringFestival(availabilityDate)) {
+				EventDate eventAvailabilityDate = new EventDate(availabilityDate);
+				Availability availability = new Availability(eventAvailabilityDate);
+				availabilities.add(availability);
 			}
 		}
 		return availabilities;
 	}
-	
+
 	private boolean availabilityDateIsDuringFestival(LocalDate date) {
-		return date.isAfter(EventDate.START_DATE.minusDays(1)) &&
-				date.isBefore(EventDate.END_DATE.plusDays(1));
+		LocalDate startMinusOneDay = EventDate.START_DATE.minusDays(1);
+		LocalDate endPlusOneDay = EventDate.END_DATE.plusDays(1);
+		return date.isAfter(startMinusOneDay) && date.isBefore(endPlusOneDay);
 	}
 
 }
--- booking-api/src/main/java/ca/ulaval/glo4002/booking/artists/ArtistService.java
+++ booking-api/src/main/java/ca/ulaval/glo4002/booking/artists/ArtistService.java
@@ -12,57 +12,57 @@ import javax.inject.Inject;
 import ca.ulaval.glo4002.booking.profits.Money;
 
 public class ArtistService {
-	
-    private final ArtistRepository artistRepository;
-    private final ArtistConverter artistConverter;
 
-    @Inject
-    public ArtistService(ArtistRepository artistRepository) {
-        this.artistRepository = artistRepository;
-        this.artistConverter = new ArtistConverter(artistRepository);
-        artistConverter.convert();
-        
-    }
+	private final ArtistRepository repository;
+	private final ArtistConverter converter;
 
-    public ArtistListDto getAllOrdered(String orderBy) {
-    	
-    	List<BookingArtist> bookingArtists = artistRepository.findAll();
-    	List<String> artistNames = new ArrayList<>();
-    	
-        if(orderBy.equalsIgnoreCase(ArtistOrderings.LOW_COSTS.toString())) {
-        	orderByLowCost(bookingArtists);
-        	artistNames.addAll(getArtistNames(bookingArtists));
-        } else if (orderBy.equalsIgnoreCase(ArtistOrderings.MOST_POPULAR.toString())) {
-        	orderByMostPopular(bookingArtists);
-        	artistNames.addAll(getArtistNames(bookingArtists));
-        } else  {
-        	artistNames.addAll(getArtistNames(bookingArtists));
-        }
-        
-        return new ArtistListDto(artistNames);
-    }
-    
-    public ArtistListDto getAllUnordered() {
-    	
-    	List<BookingArtist> bookingArtists = artistRepository.findAll();
-    	return new ArtistListDto(getArtistNames(bookingArtists));
-    	
-    }
+	@Inject
+	public ArtistService(ArtistRepository repository) {
+		this.repository = repository;
+		this.converter = new ArtistConverter(repository);
+		converter.convert();
 
-    private List<String> getArtistNames(List<BookingArtist> artists) {
-        return artists.stream().map(BookingArtist::getName).collect(Collectors.toList());
-    }
+	}
 
-    private List<BookingArtist> orderByMostPopular(List<BookingArtist> artists) {
-    	Collections.
-    	sort(artists, (artist1, artist2) -> artist1.getPopularityRank() - artist2.getPopularityRank());
-        return artists;
-    }
+	public ArtistListDto getAllOrdered(String orderBy) {
 
-    private List<BookingArtist> orderByLowCost(List<BookingArtist> artists) {
-    	artists.sort(Comparator.comparing(((Function<BookingArtist, Money>)BookingArtist::getCost).
-    			andThen(Money::getValue)).
-    			thenComparing(BookingArtist::getPopularityRank));
-        return artists;
-    }
+		List<BookingArtist> bookingArtists = repository.findAll();
+		List<String> artistNames = new ArrayList<>();
+
+		if (orderBy.equalsIgnoreCase(ArtistOrderings.LOW_COSTS.toString())) {
+			orderByLowCost(bookingArtists);
+			artistNames.addAll(getArtistNames(bookingArtists));
+		} else if (orderBy.equalsIgnoreCase(ArtistOrderings.MOST_POPULAR.toString())) {
+			orderByMostPopular(bookingArtists);
+			artistNames.addAll(getArtistNames(bookingArtists));
+		} else {
+			artistNames.addAll(getArtistNames(bookingArtists));
+		}
+
+		return new ArtistListDto(artistNames);
+	}
+
+	public ArtistListDto getAllUnordered() {
+
+		List<BookingArtist> bookingArtists = repository.findAll();
+		List<String> artistNames = getArtistNames(bookingArtists);
+		return new ArtistListDto(artistNames);
+
+	}
+
+	private List<String> getArtistNames(List<BookingArtist> artists) {
+		return artists.stream().map(BookingArtist::getName).collect(Collectors.toList());
+	}
+
+	private List<BookingArtist> orderByMostPopular(List<BookingArtist> artists) {
+		Collections.sort(artists, (artist1, artist2) -> artist1.getPopularityRank() - artist2.getPopularityRank());
+		return artists;
+	}
+
+	private List<BookingArtist> orderByLowCost(List<BookingArtist> artists) {
+		artists.sort(
+				Comparator.comparing(((Function<BookingArtist, Money>) BookingArtist::getCost).andThen(Money::getValue))
+						.thenComparing(BookingArtist::getPopularityRank));
+		return artists;
+	}
 }
