740688868460766b0321c452c6937cfdaa14f0b8
MonsterX9797 <m-cgo@hotmail.com>
2019-11-15 15:28:16 -0500

    errorMapper

---
+++ booking-api/src/main/java/ca/ulaval/glo4002/booking/exceptions/ExceptionMapper.java
@@ -0,0 +1,20 @@
+package ca.ulaval.glo4002.booking.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import ca.ulaval.glo4002.booking.errors.ErrorDto;
+
+public class ExceptionMapper {
+
+	public ResponseEntity mapError(Exception exception) {
+		if (exception instanceof BookingException) {
+			BookingException bookingException = (BookingException) exception;
+			ErrorDto errorDto = bookingException.toErrorDto();
+			HttpStatus status = bookingException.getStatus();
+			return ResponseEntity.status(status).body(errorDto);
+		} else {
+			return ResponseEntity.badRequest().build();
+		}
+	}
+}
