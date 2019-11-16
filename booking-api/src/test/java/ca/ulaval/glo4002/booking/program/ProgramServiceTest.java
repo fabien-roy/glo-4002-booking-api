ff1482f31250b325695e942867dc71e6c4d4c9da
Fabien Roy <fabienroy28@gmail.com>
2019-11-15 19:44:29 -0500

    Add ProgramService test names

--- booking-api/src/test/java/ca/ulaval/glo4002/booking/program/ProgramServiceTest.java
+++ booking-api/src/test/java/ca/ulaval/glo4002/booking/program/ProgramServiceTest.java
@@ -1,6 +1,54 @@
 package ca.ulaval.glo4002.booking.program;
 
+import ca.ulaval.glo4002.booking.events.EventFactory;
+import ca.ulaval.glo4002.booking.events.EventRepository;
+import ca.ulaval.glo4002.booking.oxygen.inventory.OxygenInventoryService;
+import ca.ulaval.glo4002.booking.shuttles.trips.TripService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.mockito.Mockito.mock;
+
 class ProgramServiceTest {
 
-    // TODO
+    ProgramService service;
+    private EventRepository eventRepository;
+    private EventFactory eventFactory;
+    private TripService tripService;
+    private OxygenInventoryService oxygenInventoryService;
+
+    @BeforeEach
+    void setUpService() {
+        eventRepository = mock(EventRepository.class);
+        eventFactory = mock(EventFactory.class);
+        tripService = mock(TripService.class);
+        oxygenInventoryService = mock(OxygenInventoryService.class);
+
+        service = new ProgramService(eventRepository, eventFactory, tripService, oxygenInventoryService);
+    }
+
+    @Test
+    void add_shouldSaveEventsToRepository() {
+        // TODO
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2, 3})
+    void add_shouldOrderTripsForEachArtist(int eventQuantity) {
+        // TODO
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2, 3})
+    void add_shouldOrderOxygenTanksForEachArtist(int eventQuantity) {
+        // TODO
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2, 3})
+    void add_shouldOrderOxygenTanksForEachActivity(int eventQuantity) {
+        // TODO
+    }
 }
