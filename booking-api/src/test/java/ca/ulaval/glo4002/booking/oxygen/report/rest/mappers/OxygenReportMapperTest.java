a1d40da9c12285139553f8077a57b58d1612a0cd
Fabien Roy <fabienroy28@gmail.com>
2019-11-29 12:21:12 -0500

    Add oxygen report mapper tests

--- booking-api/src/test/java/ca/ulaval/glo4002/booking/oxygen/report/rest/mappers/OxygenReportMapperTest.java
+++ booking-api/src/test/java/ca/ulaval/glo4002/booking/oxygen/report/rest/mappers/OxygenReportMapperTest.java
@@ -1,14 +1,23 @@
 package ca.ulaval.glo4002.booking.oxygen.report.rest.mappers;
 
 import ca.ulaval.glo4002.booking.oxygen.history.domain.OxygenHistory;
+import ca.ulaval.glo4002.booking.oxygen.history.rest.OxygenHistoryItemDto;
 import ca.ulaval.glo4002.booking.oxygen.history.rest.mappers.OxygenHistoryMapper;
 import ca.ulaval.glo4002.booking.oxygen.inventory.domain.OxygenInventory;
+import ca.ulaval.glo4002.booking.oxygen.inventory.rest.OxygenInventoryItemDto;
 import ca.ulaval.glo4002.booking.oxygen.inventory.rest.mappers.OxygenInventoryMapper;
 import ca.ulaval.glo4002.booking.oxygen.report.domain.OxygenReport;
+import ca.ulaval.glo4002.booking.oxygen.report.rest.OxygenReportResponse;
 import ca.ulaval.glo4002.booking.oxygen.report.rest.mappers.OxygenReportMapper;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
-import static org.mockito.Mockito.mock;
+import java.util.Collections;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.*;
 
 class OxygenReportMapperTest {
 
@@ -24,5 +33,41 @@ class OxygenReportMapperTest {
         oxygenReportMapper = new OxygenReportMapper(inventoryMapper, historyMapper);
     }
 
-    // TODO : OxygenReportMapperTest
+    @Test
+    void toResponse_shouldCallInventoryMapperWithInventory() {
+        OxygenInventory inventory = mock(OxygenInventory.class);
+
+        oxygenReportMapper.toResponse(inventory, mock(OxygenHistory.class));
+
+        verify(inventoryMapper).toDto(eq(inventory));
+    }
+
+    @Test
+    void toResponse_shouldCallHistoryMapperWithHistory() {
+        OxygenHistory history = mock(OxygenHistory.class);
+
+        oxygenReportMapper.toResponse(mock(OxygenInventory.class), history);
+
+        verify(historyMapper).toDto(eq(history));
+    }
+
+    @Test
+    void toResponse_shouldReturnReportWithInventory() {
+        List<OxygenInventoryItemDto> expectedInventoryResponse = Collections.singletonList(mock(OxygenInventoryItemDto.class));
+        when(inventoryMapper.toDto(any())).thenReturn(expectedInventoryResponse);
+
+        OxygenReportResponse response = oxygenReportMapper.toResponse(mock(OxygenInventory.class), mock(OxygenHistory.class));
+
+        assertEquals(expectedInventoryResponse.get(0), response.getInventory().get(0));
+    }
+
+    @Test
+    void toResponse_shouldReturnReportWithHistory() {
+        List<OxygenHistoryItemDto> expectedHistoryResponse = Collections.singletonList(mock(OxygenHistoryItemDto.class));
+        when(historyMapper.toDto(any())).thenReturn(expectedHistoryResponse);
+
+        OxygenReportResponse response = oxygenReportMapper.toResponse(mock(OxygenInventory.class), mock(OxygenHistory.class));
+
+        assertEquals(expectedHistoryResponse.get(0), response.getHistory().get(0));
+    }
 }
