370f1997da5731f6cb495997488e78977c951dd0
Fabien Roy <fabienroy28@gmail.com>
2019-11-30 16:44:43 -0500

    Remove empty categories from inventory response

--- booking-api/src/main/java/ca/ulaval/glo4002/booking/oxygen/inventory/rest/mappers/OxygenInventoryMapper.java
+++ booking-api/src/main/java/ca/ulaval/glo4002/booking/oxygen/inventory/rest/mappers/OxygenInventoryMapper.java
@@ -1,24 +1,26 @@
 package ca.ulaval.glo4002.booking.oxygen.inventory.rest.mappers;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
 import ca.ulaval.glo4002.booking.oxygen.domain.OxygenCategories;
 import ca.ulaval.glo4002.booking.oxygen.inventory.domain.OxygenInventory;
 import ca.ulaval.glo4002.booking.oxygen.inventory.rest.OxygenInventoryItemResponse;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class OxygenInventoryMapper {
 
+    // TODO : Test that item is not there when quantity is 0
+
 	public List<OxygenInventoryItemResponse> toResponse(OxygenInventory inventory) {
-		Long eOxygenTankQuantity = inventory.getAllQuantityByCategory(OxygenCategories.E).longValue();
-		Long bOxygenTankQuantity = inventory.getAllQuantityByCategory(OxygenCategories.B).longValue();
-		Long aOxygenTankQuantity = inventory.getAllQuantityByCategory(OxygenCategories.A).longValue();
+		long eOxygenTankQuantity = inventory.getAllQuantityByCategory(OxygenCategories.E).longValue();
+		long bOxygenTankQuantity = inventory.getAllQuantityByCategory(OxygenCategories.B).longValue();
+		long aOxygenTankQuantity = inventory.getAllQuantityByCategory(OxygenCategories.A).longValue();
 
-		OxygenInventoryItemResponse eGradeResponse = new OxygenInventoryItemResponse(OxygenCategories.E.toString(), eOxygenTankQuantity);
-		OxygenInventoryItemResponse bGradeResponse = new OxygenInventoryItemResponse(OxygenCategories.B.toString(), bOxygenTankQuantity);
-		OxygenInventoryItemResponse aGradeResponse = new OxygenInventoryItemResponse(OxygenCategories.A.toString(), aOxygenTankQuantity);
+		List<OxygenInventoryItemResponse> responses = new ArrayList<>();
+		if (eOxygenTankQuantity > 0) responses.add(new OxygenInventoryItemResponse(OxygenCategories.E.toString(), eOxygenTankQuantity));
+		if (bOxygenTankQuantity > 0) responses.add(new OxygenInventoryItemResponse(OxygenCategories.B.toString(), bOxygenTankQuantity));
+		if (aOxygenTankQuantity > 0) responses.add(new OxygenInventoryItemResponse(OxygenCategories.A.toString(), aOxygenTankQuantity));
 
-		return new ArrayList<>(Arrays.asList(eGradeResponse, bGradeResponse, aGradeResponse));
+		return responses;
 	}
 }
