6f5c6c3d7dff8272b6362b028e0584eb368f7409
Fabien Roy <fabienroy28@gmail.com>
2019-12-10 22:16:07 -0500

    Add public to wtv wtv blabla

--- booking-api/src/test/java/ca/ulaval/glo4002/booking/passes/domain/pricediscountstrategy/NoPriceDiscountStrategyTest.java
+++ booking-api/src/test/java/ca/ulaval/glo4002/booking/passes/domain/pricediscountstrategy/NoPriceDiscountStrategyTest.java
@@ -1,18 +1,17 @@
 package ca.ulaval.glo4002.booking.passes.domain.pricediscountstrategy;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.Mockito.mock;
+import ca.ulaval.glo4002.booking.passes.domain.Pass;
+import ca.ulaval.glo4002.booking.profits.domain.Money;
+import org.junit.jupiter.api.Test;
 
 import java.math.BigDecimal;
 import java.util.Collections;
 import java.util.List;
 
-import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.mock;
 
-import ca.ulaval.glo4002.booking.profits.domain.Money;
-import ca.ulaval.glo4002.booking.passes.domain.Pass;
-
-class NoPriceDiscountStrategyTest {
+public class NoPriceDiscountStrategyTest {
 
     @Test
     void calculateDiscount_shouldReturnPassPrice() {
