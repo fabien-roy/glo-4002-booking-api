49a33d6d96bfbc591f4a068339048830be5f01e0
Fabien Roy <fabienroy28@gmail.com>
2019-12-10 23:27:22 -0500

    Refactor OrderServiceTest

--- booking-api/src/test/java/ca/ulaval/glo4002/booking/orders/services/OrderServiceTest.java
+++ booking-api/src/test/java/ca/ulaval/glo4002/booking/orders/services/OrderServiceTest.java
@@ -1,77 +1,73 @@
 package ca.ulaval.glo4002.booking.orders.services;
 
-import ca.ulaval.glo4002.booking.festival.domain.FestivalConfiguration;
+import ca.ulaval.glo4002.booking.numbers.Number;
 import ca.ulaval.glo4002.booking.orders.domain.Order;
 import ca.ulaval.glo4002.booking.orders.domain.OrderDate;
 import ca.ulaval.glo4002.booking.orders.domain.OrderFactory;
 import ca.ulaval.glo4002.booking.orders.domain.OrderNumber;
 import ca.ulaval.glo4002.booking.orders.infrastructure.OrderRepository;
 import ca.ulaval.glo4002.booking.orders.rest.OrderRequest;
-import ca.ulaval.glo4002.booking.orders.rest.OrderResponse;
 import ca.ulaval.glo4002.booking.orders.rest.mappers.OrderMapper;
-import ca.ulaval.glo4002.booking.program.events.domain.EventDate;
-import ca.ulaval.glo4002.booking.numbers.Number;
 import ca.ulaval.glo4002.booking.oxygen.inventory.services.OxygenInventoryService;
 import ca.ulaval.glo4002.booking.passes.domain.Pass;
-import ca.ulaval.glo4002.booking.passes.domain.PassCategories;
-import ca.ulaval.glo4002.booking.passes.domain.PassCategory;
-import ca.ulaval.glo4002.booking.passes.domain.PassOptions;
 import ca.ulaval.glo4002.booking.passes.domain.PassBundle;
-import ca.ulaval.glo4002.booking.passes.rest.PassBundleRequest;
-import ca.ulaval.glo4002.booking.passes.rest.mappers.PassBundleMapper;
-import ca.ulaval.glo4002.booking.profits.domain.Money;
+import ca.ulaval.glo4002.booking.passes.domain.PassCategories;
 import ca.ulaval.glo4002.booking.shuttles.trips.services.TripService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import java.math.BigDecimal;
-import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.Mockito.any;
-import static org.mockito.Mockito.eq;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 class OrderServiceTest {
+
+    // TODO : Review OrderServiceTest when called services will receive Order
 
 	private OrderService service;
 	private OrderRepository repository;
 	private OrderFactory factory;
 	private TripService tripService;
 	private OxygenInventoryService oxygenInventoryService;
+	private Order order;
+	private PassBundle passBundle; // TODO : Useless when services called by OrderService will receive Order
 
 	@BeforeEach
 	void setUpService() {
-		OrderMapper mapper = new OrderMapper(new PassBundleMapper());
+		OrderMapper mapper = mock(OrderMapper.class);
 		repository = mock(OrderRepository.class);
-		factory = mock(OrderFactory.class);
 		tripService = mock(TripService.class);
 		oxygenInventoryService = mock(OxygenInventoryService.class);
 
 		service = new OrderService(repository, factory, mapper, tripService, oxygenInventoryService);
 	}
 
+	@BeforeEach
+	void setUpOrder() {
+	    passBundle = mock(PassBundle.class);
+		OrderNumber orderNumber = mock(OrderNumber.class);
+		order = mock(Order.class);
+		when(order.getOrderNumber()).thenReturn(orderNumber);
+		when(order.getPassBundle()).thenReturn(passBundle);
+		factory = mock(OrderFactory.class);
+		when(factory.create(any())).thenReturn(order);
+	}
+
 	@Test
 	void order_shouldAddOrder() {
-		String aVendorCode = "aVendorCode";
-		Order anOrder = mockOrder(aVendorCode, PassCategories.SUPERNOVA);
-		OrderRequest orderRequest = mockOrderRequest(aVendorCode);
+		OrderRequest orderRequest = mock(OrderRequest.class);
 
 		service.order(orderRequest);
 
-		verify(repository).addOrder(eq(anOrder));
+		verify(repository).addOrder(eq(order));
 	}
 
 	@Test
 	void order_shouldOrderTripsWithCorrectPassCategory() {
-		String aVendorCode = "aVendorCode";
-		OrderRequest orderRequest = mockOrderRequest(aVendorCode);
+		OrderRequest orderRequest = mock(OrderRequest.class);
 		PassCategories expectedPassCategory = PassCategories.SUPERNOVA;
-		mockOrder(aVendorCode, expectedPassCategory);
+		when(passBundle.getCategory()).thenReturn(expectedPassCategory);
 
 		service.order(orderRequest);
 
@@ -80,10 +76,9 @@ class OrderServiceTest {
 
 	@Test
 	void order_shouldOrderTripsWithCorrectPasses() {
-		String aVendorCode = "aVendorCode";
-		OrderRequest orderRequest = mockOrderRequest(aVendorCode);
-		List<Pass> expectedPasses = new ArrayList<>();
-		mockOrder(aVendorCode, PassCategories.SUPERNOVA, expectedPasses);
+		OrderRequest orderRequest = mock(OrderRequest.class);
+		List<Pass> expectedPasses = Collections.singletonList(mock(Pass.class));
+		when(order.getPasses()).thenReturn(expectedPasses);
 
 		service.order(orderRequest);
 
@@ -92,10 +87,9 @@ class OrderServiceTest {
 
 	@Test
 	void order_shouldOrderOxygenWithCorrectPassCategory() {
-		String aVendorCode = "aVendorCode";
-		OrderRequest orderRequest = mockOrderRequest(aVendorCode);
+		OrderRequest orderRequest = mock(OrderRequest.class);
 		PassCategories expectedPassCategory = PassCategories.SUPERNOVA;
-		mockOrder(aVendorCode, expectedPassCategory);
+		when(passBundle.getCategory()).thenReturn(expectedPassCategory);
 
 		service.order(orderRequest);
 
@@ -104,10 +98,9 @@ class OrderServiceTest {
 
 	@Test
 	void order_shouldOrderOxygenWithCorrectPasses() {
-		String aVendorCode = "aVendorCode";
-		OrderRequest orderRequest = mockOrderRequest(aVendorCode);
-		List<Pass> expectedPasses = new ArrayList<>();
-		mockOrder(aVendorCode, PassCategories.SUPERNOVA, expectedPasses);
+		OrderRequest orderRequest = mock(OrderRequest.class);
+		List<Pass> expectedPasses = Collections.singletonList(mock(Pass.class));
+		when(order.getPasses()).thenReturn(expectedPasses);
 
 		service.order(orderRequest);
 
@@ -115,43 +108,24 @@ class OrderServiceTest {
 	}
 
 	@Test
+	void order_shouldOrderOxygenWithCorrectDate() {
+		OrderRequest orderRequest = mock(OrderRequest.class);
+		OrderDate expectedOrderDate = mock(OrderDate.class);
+		when(order.getOrderDate()).thenReturn(expectedOrderDate);
+
+		service.order(orderRequest);
+
+		verify(oxygenInventoryService).orderForPasses(any(), any(), eq(expectedOrderDate));
+	}
+
+	@Test
 	void getByOrderNumber_shouldGetOrder() {
-		OrderNumber anOrderNumber = new OrderNumber(new Number(1L), "Vendor");
-		Pass aPass = mock(Pass.class);
-		when(aPass.getPassNumber()).thenReturn(new Number(1L));
-		when(aPass.getPrice()).thenReturn(new Money(new BigDecimal(0.0)));
-		PassBundle passBundle = new PassBundle(
-				Collections.singletonList(aPass),
-				new PassCategory(PassCategories.SUPERNOVA, null),
-				PassOptions.PACKAGE
-		);
-		OrderDate aOrderDate = new OrderDate(FestivalConfiguration.getDefaultStartEventDate().minusDays(1).toLocalDateTime());
-		Order order = new Order(anOrderNumber, aOrderDate, passBundle);
-		when(repository.getByOrderNumber(anOrderNumber)).thenReturn(order);
+		OrderNumber orderNumber = new OrderNumber(new Number(1L), "VENDOR");
+	    when(order.getOrderNumber()).thenReturn(orderNumber);
+	    when(repository.getByOrderNumber(order.getOrderNumber())).thenReturn(order);
 
-		OrderResponse orderResponse = service.getByOrderNumber(anOrderNumber.toString());
+		service.getByOrderNumber(order.getOrderNumber().toString());
 
-		assertEquals(order.getPrice().getValue().doubleValue(), orderResponse.getOrderPrice());
-	}
-
-	private Order mockOrder(String vendorCode, PassCategories passCategory) {
-		return mockOrder(vendorCode, passCategory, new ArrayList<>());
-	}
-
-	private Order mockOrder(String vendorCode, PassCategories passCategory, List<Pass> passes) {
-		Order order = mock(Order.class);
-		when(order.getOrderNumber()).thenReturn(new OrderNumber(new Number(1L), vendorCode));
-
-		PassBundle passBundle = mock(PassBundle.class);
-		when(passBundle.getCategory()).thenReturn(passCategory);
-		when(passBundle.getPasses()).thenReturn(passes);
-		when(order.getPassBundle()).thenReturn(passBundle);
-		when(factory.create(any())).thenReturn(order);
-
-		return order;
-	}
-
-	private OrderRequest mockOrderRequest(String vendorCode) {
-		return new OrderRequest("aOrderDate", vendorCode, mock(PassBundleRequest.class));
+		verify(repository).getByOrderNumber(eq(order.getOrderNumber()));
 	}
 }
