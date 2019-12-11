b224c9ba7b9c255314b198e82e76c29e684fe244
Fabien Roy <fabienroy28@gmail.com>
2019-12-10 21:58:57 -0500

    Repair InMemoryEventRepositoryTest

--- booking-api/src/test/java/ca/ulaval/glo4002/booking/program/events/infrastructure/InMemoryEventRepositoryTest.java
+++ booking-api/src/test/java/ca/ulaval/glo4002/booking/program/events/infrastructure/InMemoryEventRepositoryTest.java
@@ -1,16 +1,13 @@
 package ca.ulaval.glo4002.booking.program.events.infrastructure;
 
 import ca.ulaval.glo4002.booking.program.events.domain.Event;
-import ca.ulaval.glo4002.booking.program.events.infrastructure.EventRepository;
-import ca.ulaval.glo4002.booking.program.events.infrastructure.InMemoryEventRepository;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.ValueSource;
+import org.junit.jupiter.api.Test;
 
-import java.util.Collections;
+import java.util.Arrays;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.mock;
 
 class InMemoryEventRepositoryTest {
@@ -22,14 +19,16 @@ class InMemoryEventRepositoryTest {
         repository = new InMemoryEventRepository();
     }
 
-    @ParameterizedTest
-    @ValueSource(ints = {1, 2, 3})
-    void addAll_shouldSaveAllEvents(int amountOfEvents) {
-        List<Event> events = Collections.nCopies(amountOfEvents, mock(Event.class));
+    @Test
+    void addAll_shouldSaveAllEvents() {
+        Event expectedEvent = mock(Event.class);
+        Event otherExpectedEvent = mock(Event.class);
+        List<Event> events = Arrays.asList(expectedEvent, otherExpectedEvent);
 
         repository.addAll(events);
         List<Event> returnedEvents = repository.findAll();
 
-        assertEquals(amountOfEvents, returnedEvents.size());
+        assertTrue(returnedEvents.stream().anyMatch(expectedEvent::equals));
+        assertTrue(returnedEvents.stream().anyMatch(otherExpectedEvent::equals));
     }
 }
