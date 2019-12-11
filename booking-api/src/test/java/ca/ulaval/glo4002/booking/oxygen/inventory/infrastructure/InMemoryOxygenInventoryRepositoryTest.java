161644b5e695fdf678804ded3639b2b43d3dfb0b
Fabien Roy <fabienroy28@gmail.com>
2019-12-10 22:03:51 -0500

    Add test for InMemoryOxygenInventoryRepository.findAll

--- booking-api/src/test/java/ca/ulaval/glo4002/booking/oxygen/inventory/infrastructure/InMemoryOxygenInventoryRepositoryTest.java
+++ booking-api/src/test/java/ca/ulaval/glo4002/booking/oxygen/inventory/infrastructure/InMemoryOxygenInventoryRepositoryTest.java
@@ -1,12 +1,17 @@
 package ca.ulaval.glo4002.booking.oxygen.inventory.infrastructure;
 
-import static org.junit.jupiter.api.Assertions.*;
-
+import ca.ulaval.glo4002.booking.oxygen.domain.OxygenCategories;
+import ca.ulaval.glo4002.booking.oxygen.domain.OxygenTank;
 import ca.ulaval.glo4002.booking.oxygen.inventory.domain.OxygenInventory;
-import ca.ulaval.glo4002.booking.oxygen.inventory.infrastructure.InMemoryOxygenInventoryRepository;
-import ca.ulaval.glo4002.booking.oxygen.inventory.infrastructure.OxygenInventoryRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.util.Collections;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.mock;
 
 class InMemoryOxygenInventoryRepositoryTest {
 
@@ -25,5 +30,17 @@ class InMemoryOxygenInventoryRepositoryTest {
 		OxygenInventory inventory = repository.getInventory();
 
 		assertEquals(expectedInventory, inventory);
+	}
+
+	@Test
+	void findAll_shouldReturnAllTanks() {
+	    OxygenTank expectedOxygenTank = mock(OxygenTank.class);
+		OxygenInventory inventory = new OxygenInventory();
+		inventory.addTanksToInventory(OxygenCategories.E, Collections.singletonList(expectedOxygenTank));
+
+		repository.setInventory(inventory);
+		List<OxygenTank> oxygenTanks = repository.findAll();
+
+		assertTrue(oxygenTanks.stream().allMatch(expectedOxygenTank::equals));
 	}
 }
