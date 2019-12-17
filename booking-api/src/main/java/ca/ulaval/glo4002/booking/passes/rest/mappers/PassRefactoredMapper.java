ea13454204f21b87c60a25a75751ad7fdaaa91d3
Fabien Roy <fabienroy28@gmail.com>
2019-12-16 20:43:14 -0500

    Add PassRefactoredMapper logic for option and category

--- booking-api/src/main/java/ca/ulaval/glo4002/booking/passes/rest/mappers/PassRefactoredMapper.java
+++ booking-api/src/main/java/ca/ulaval/glo4002/booking/passes/rest/mappers/PassRefactoredMapper.java
@@ -1,15 +1,30 @@
 package ca.ulaval.glo4002.booking.passes.rest.mappers;
 
+import ca.ulaval.glo4002.booking.passes.domain.PassCategories;
+import ca.ulaval.glo4002.booking.passes.domain.PassOptions;
 import ca.ulaval.glo4002.booking.passes.domain.PassRefactored;
 import ca.ulaval.glo4002.booking.passes.rest.PassRefactoredRequest;
 
+import java.util.Collections;
+
 public class PassRefactoredMapper {
 
-    // TODO : Use EventDateMapper (is currently EventDateFactory)
-
     // TODO : Handle price calculation
+    // TODO : Use EventDateMapper (is currently EventDateFactory)
+    // TODO : Set arrival and departure dates
 
-    public PassRefactored fromRequest(PassRefactoredRequest passBundle) {
-        return null; // TODO : Return PassRefactored
+    public PassRefactored fromRequest(PassRefactoredRequest request) {
+        PassCategories category = parsePassCategory(request.getPassCategory());
+        PassOptions option = parsePassOption(request.getPassOption());
+
+        return new PassRefactored(category, option, null, Collections.emptyList(), Collections.emptyList(), Collections.emptyList());
+    }
+
+    private PassCategories parsePassCategory(String category) {
+        return PassCategories.get(category);
+    }
+
+    private PassOptions parsePassOption(String option) {
+        return PassOptions.get(option);
     }
 }
