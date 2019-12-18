20b5e50b382c084da19f9c48ab88edd35b419a1a
Fabien Roy <fabienroy28@gmail.com>
2019-12-18 00:37:57 -0500

    Fix imports

--- booking-api/src/main/java/ca/ulaval/glo4002/booking/orders/rest/mappers/OrderMapper.java
+++ booking-api/src/main/java/ca/ulaval/glo4002/booking/orders/rest/mappers/OrderMapper.java
@@ -3,6 +3,7 @@ package ca.ulaval.glo4002.booking.orders.rest.mappers;
 import ca.ulaval.glo4002.booking.interfaces.rest.exceptions.InvalidFormatException;
 import ca.ulaval.glo4002.booking.orders.domain.Order;
 import ca.ulaval.glo4002.booking.orders.domain.OrderDate;
+import ca.ulaval.glo4002.booking.orders.domain.OrderRefactored;
 import ca.ulaval.glo4002.booking.orders.rest.OrderRefactoredRequest;
 import ca.ulaval.glo4002.booking.orders.rest.OrderResponse;
 import ca.ulaval.glo4002.booking.passes.domain.PassRefactored;
