8961cbdbd26bc8a700056af753ff182ad32b4716
Fabien Roy <fabienroy28@gmail.com>
2019-12-19 12:27:28 -0500

    Fix Shuttle Manifest Mapper tests

--- booking-api/src/test/java/ca/ulaval/glo4002/booking/shuttles/rest/mappers/ShuttleManifestMapperTest.java
+++ booking-api/src/test/java/ca/ulaval/glo4002/booking/shuttles/rest/mappers/ShuttleManifestMapperTest.java
@@ -1,13 +1,17 @@
 package ca.ulaval.glo4002.booking.shuttles.rest.mappers;
 
+import ca.ulaval.glo4002.booking.shuttles.rest.ShuttleManifestResponse;
 import ca.ulaval.glo4002.booking.shuttles.trips.domain.Trip;
+import ca.ulaval.glo4002.booking.shuttles.trips.rest.TripResponse;
 import ca.ulaval.glo4002.booking.shuttles.trips.rest.mappers.TripMapper;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.*;
 
 class ShuttleManifestMapperTest {
@@ -23,12 +27,26 @@ class ShuttleManifestMapperTest {
     }
 
     @Test
-    void toResponse_shouldMapArrivalsAndDepartures() {
+    void toResponse_shouldMapArrivals() {
         List<Trip> arrivals = new ArrayList<>();
+        TripResponse expectedArrivalResponse = mock(TripResponse.class);
+        List<TripResponse> expectedArrivalResponses = Collections.singletonList(expectedArrivalResponse);
+        when(tripMapper.toResponse(arrivals)).thenReturn(expectedArrivalResponses);
+
+        ShuttleManifestResponse response = shuttleManifestMapper.toResponse(arrivals, new ArrayList<>());
+
+        assertEquals(expectedArrivalResponse, response.getArrivals().get(0));
+    }
+
+    @Test
+    void toResponse_shouldMapDepartures() {
         List<Trip> departures = new ArrayList<>();
+        TripResponse expectedDepartureResponse = mock(TripResponse.class);
+        List<TripResponse> expectedDepartureResponses = Collections.singletonList(expectedDepartureResponse);
+        when(tripMapper.toResponse(departures)).thenReturn(expectedDepartureResponses);
 
-        shuttleManifestMapper.toResponse(arrivals, departures);
+        ShuttleManifestResponse response = shuttleManifestMapper.toResponse(new ArrayList<>(), departures);
 
-        verify(tripMapper, times(2)).toResponse(any());
+        assertEquals(expectedDepartureResponse, response.getDepartures().get(0));
     }
 }
