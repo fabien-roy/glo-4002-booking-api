cabc69bc3eb70073613332d4af57fcf1a0b845c0
Fabien Roy <fabienroy28@gmail.com>
2019-12-19 12:13:34 -0500

    Remove parametrized tests in Program Service

--- booking-api/src/test/java/ca/ulaval/glo4002/booking/program/services/ProgramServiceTest.java
+++ booking-api/src/test/java/ca/ulaval/glo4002/booking/program/services/ProgramServiceTest.java
@@ -11,8 +11,6 @@ import ca.ulaval.glo4002.booking.program.rest.ProgramRequest;
 import ca.ulaval.glo4002.booking.shuttles.trips.services.TripService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.Collections;
 import java.util.List;
@@ -53,35 +51,65 @@ class ProgramServiceTest {
         verify(eventRepository).addAll(eq(expectedEvents));
     }
 
-    @ParameterizedTest
-    @ValueSource(ints = {1, 2, 3})
-    void add_shouldOrderTripsForEachArtist(int eventQuantity) {
+    @Test
+    void add_shouldOrderTripsForArtist_whenThereIsASingleArtist() {
         ProgramRequest aProgramRequest = mock(ProgramRequest.class);
         Event aEvent = mock(Event.class);
         Artist expectedArtist = mock(Artist.class);
         EventDate expectedEventDate = mock(EventDate.class);
         when(aEvent.getArtist()).thenReturn(expectedArtist);
         when(aEvent.getEventDate()).thenReturn(expectedEventDate);
-        when(eventFactory.create(any())).thenReturn(Collections.nCopies(eventQuantity, aEvent));
+        when(eventFactory.create(any())).thenReturn(Collections.singletonList(aEvent));
 
         service.add(aProgramRequest);
 
-        verify(tripService, times(eventQuantity)).orderForArtist(eq(expectedArtist), eq(expectedEventDate));
+        verify(tripService).orderForArtist(eq(expectedArtist), eq(expectedEventDate));
     }
 
-    @ParameterizedTest
-    @ValueSource(ints = {1, 2, 3})
-    void add_shouldOrderOxygenTanksForEachArtist(int eventQuantity) {
+    @Test
+    void add_shouldOrderTripsForArtists_whenThereAreMultipleArtists() {
+        int artistQuantity = 2;
         ProgramRequest aProgramRequest = mock(ProgramRequest.class);
         Event aEvent = mock(Event.class);
         Artist expectedArtist = mock(Artist.class);
         EventDate expectedEventDate = mock(EventDate.class);
         when(aEvent.getArtist()).thenReturn(expectedArtist);
         when(aEvent.getEventDate()).thenReturn(expectedEventDate);
-        when(eventFactory.create(any())).thenReturn(Collections.nCopies(eventQuantity, aEvent));
+        when(eventFactory.create(any())).thenReturn(Collections.nCopies(artistQuantity, aEvent));
 
         service.add(aProgramRequest);
 
-        verify(oxygenInventoryService, times(eventQuantity)).orderForArtist(eq(expectedArtist), eq(expectedEventDate));
+        verify(tripService, times(artistQuantity)).orderForArtist(eq(expectedArtist), eq(expectedEventDate));
+    }
+
+    @Test
+    void add_shouldOrderOxygenTanksForArtist_whenThereIsASingleArtist() {
+        ProgramRequest aProgramRequest = mock(ProgramRequest.class);
+        Event aEvent = mock(Event.class);
+        Artist expectedArtist = mock(Artist.class);
+        EventDate expectedEventDate = mock(EventDate.class);
+        when(aEvent.getArtist()).thenReturn(expectedArtist);
+        when(aEvent.getEventDate()).thenReturn(expectedEventDate);
+        when(eventFactory.create(any())).thenReturn(Collections.singletonList(aEvent));
+
+        service.add(aProgramRequest);
+
+        verify(oxygenInventoryService).orderForArtist(eq(expectedArtist), eq(expectedEventDate));
+    }
+
+    @Test
+    void add_shouldOrderOxygenTanksForArtists_whenThereAreMultipleArtists() {
+        int artistQuantity = 2;
+        ProgramRequest aProgramRequest = mock(ProgramRequest.class);
+        Event aEvent = mock(Event.class);
+        Artist expectedArtist = mock(Artist.class);
+        EventDate expectedEventDate = mock(EventDate.class);
+        when(aEvent.getArtist()).thenReturn(expectedArtist);
+        when(aEvent.getEventDate()).thenReturn(expectedEventDate);
+        when(eventFactory.create(any())).thenReturn(Collections.nCopies(artistQuantity, aEvent));
+
+        service.add(aProgramRequest);
+
+        verify(oxygenInventoryService, times(artistQuantity)).orderForArtist(eq(expectedArtist), eq(expectedEventDate));
     }
 }
