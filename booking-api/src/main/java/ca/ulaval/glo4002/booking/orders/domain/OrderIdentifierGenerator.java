63b3d1472a282d14300e2d6610d3eebbd0fbb6d0
Fabien Roy <fabienroy28@gmail.com>
2019-12-19 11:59:43 -0500

    Make generators thread safe

--- booking-api/src/main/java/ca/ulaval/glo4002/booking/orders/domain/OrderIdentifierGenerator.java
+++ booking-api/src/main/java/ca/ulaval/glo4002/booking/orders/domain/OrderIdentifierGenerator.java
@@ -2,13 +2,13 @@ package ca.ulaval.glo4002.booking.orders.domain;
 
 public class OrderIdentifierGenerator {
 
-    private OrderIdentifier nextNumber; // TODO : Make this thread safe
+    private OrderIdentifier nextNumber;
 
     public OrderIdentifierGenerator() {
         this.nextNumber = new OrderIdentifier(100000L);
     }
 
-    public OrderIdentifier generate() {
+    public synchronized  OrderIdentifier generate() {
         OrderIdentifier number = nextNumber;
         nextNumber = new OrderIdentifier(nextNumber.getValue() + 1);
 
--- booking-api/src/main/java/ca/ulaval/glo4002/booking/passes/domain/PassNumberGenerator.java
+++ booking-api/src/main/java/ca/ulaval/glo4002/booking/passes/domain/PassNumberGenerator.java
@@ -2,13 +2,13 @@ package ca.ulaval.glo4002.booking.passes.domain;
 
 public class PassNumberGenerator {
 
-    private PassNumber nextNumber; // TODO : Make this thread safe
+    private PassNumber nextNumber;
 
     public PassNumberGenerator() {
         this.nextNumber = new PassNumber(100000L);
     }
 
-    public PassNumber generate() {
+    public synchronized PassNumber generate() {
         PassNumber number = nextNumber;
 
         nextNumber = new PassNumber(nextNumber.getValue() + 1);
