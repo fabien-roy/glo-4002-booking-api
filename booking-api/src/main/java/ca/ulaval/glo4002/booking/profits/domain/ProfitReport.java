bc5f7fdaff33c9bf00729b8190fa286bd01f28f6
Fabien Roy <fabienroy28@gmail.com>
2019-12-19 23:30:31 -0500

    Remove useless methods from profit report

--- booking-api/src/main/java/ca/ulaval/glo4002/booking/profits/domain/ProfitReport.java
+++ booking-api/src/main/java/ca/ulaval/glo4002/booking/profits/domain/ProfitReport.java
@@ -1,29 +1,13 @@
 package ca.ulaval.glo4002.booking.profits.domain;
 
-import java.math.BigDecimal;
-
 public class ProfitReport {
 
     private Money expense;
     private Money revenue;
 
-    // TODO : Remove empty constructor
-    public ProfitReport() {
-        this.expense = new Money(new BigDecimal(0));
-        this.revenue = new Money(new BigDecimal(0));
-    }
-
     public ProfitReport(Money expense, Money revenue) {
         this.expense = expense;
         this.revenue = revenue;
-    }
-
-    public void addRevenue(Money revenue){
-        this.revenue = this.revenue.add(revenue);
-    }
-
-    public void addExpense(Money expense) {
-        this.expense = this.expense.add(expense);
     }
 
     public Money getExpense() {
--- booking-api/src/test/java/ca/ulaval/glo4002/booking/profits/domain/ProfitReportTest.java
+++ booking-api/src/test/java/ca/ulaval/glo4002/booking/profits/domain/ProfitReportTest.java
@@ -1,6 +1,5 @@
 package ca.ulaval.glo4002.booking.profits.domain;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.math.BigDecimal;
@@ -9,43 +8,14 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class ProfitReportTest {
 
-    private ProfitReport profitReport;
-
-    @BeforeEach
-    void setupProfit() {
-        profitReport = new ProfitReport();
-    }
-
-    @Test
-    void addExpense_shouldAddExpense() {
-        BigDecimal amount = new BigDecimal(2000);
-        Money expense = new Money(amount);
-
-        profitReport.addExpense(expense);
-
-        assertEquals(expense, profitReport.getExpense());
-    }
-
-    @Test
-    void addRevenue_shouldAddRevenue() {
-        BigDecimal amount = new BigDecimal(2000);
-        Money revenue = new Money(amount);
-
-        profitReport.addRevenue(revenue);
-
-        assertEquals(revenue, profitReport.getRevenue());
-    }
-
     @Test
     void calculateProfit_shouldSetProfitAsTheDifferenceBetweenRevenueAndExpense() {
-        BigDecimal amountRevenue = new BigDecimal(3000);
-        BigDecimal amountExpense = new BigDecimal(2000);
-        Money revenue = new Money(amountRevenue);
-        Money expense = new Money(amountExpense);
+        Money expectedTotalProfit = new Money(BigDecimal.valueOf(1000));
+        Money expense = new Money(BigDecimal.valueOf(2000));
+        Money revenue = new Money(BigDecimal.valueOf(3000));
 
-        profitReport.addRevenue(revenue);
-        profitReport.addExpense(expense);
+        ProfitReport report = new ProfitReport(expense, revenue);
 
-        assertEquals(revenue.subtract(expense), profitReport.getTotalProfit());
+        assertEquals(expectedTotalProfit, report.getTotalProfit());
     }
 }
